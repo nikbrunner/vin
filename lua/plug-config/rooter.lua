0a995333a9305c17e6cafbb72c2a8cae26e52b3d
nibru <nikolaus.brunner@protonmail.ch>
2021-09-15 11:48:31 +0200

    Tried to fix rooter

--- lua/plug-config/rooter.lua
+++ lua/plug-config/rooter.lua
@@ -5,4 +5,9 @@ require('rooter').setup({
     'package.json'
   },
   non_project_files = 'current',
+  
+  -- the start path to pass to nvim_lsp.util.root_pattern(patterns...)
+  start_path = function()
+    return vim.fn.expand [[%:p:h]]
+  end,
 })
