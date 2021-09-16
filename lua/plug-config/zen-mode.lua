1ddaadb437869e3405117528e31705acd10b1b85
nibru <nikolaus.brunner@protonmail.ch>
2021-09-16 22:04:55 +0200

    Updated Zen Mode settings

--- lua/plug-config/zen-mode.lua
+++ lua/plug-config/zen-mode.lua
@@ -1,6 +1,6 @@
 require("zen-mode").setup {
   window = {
-    backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+    backdrop = 0.75, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
     -- height and width can be:
     -- * an absolute number of cells when > 1
     -- * a percentage of the width / height of the editor when <= 1
@@ -9,9 +9,9 @@ require("zen-mode").setup {
     -- by default, no options are changed for the Zen window
     -- uncomment any of the options below, or add other vim.wo options you want to apply
     options = {
-      -- signcolumn = "no", -- disable signcolumn
-      -- number = false, -- disable number column
-      -- relativenumber = false, -- disable relative numbers
+      signcolumn = "no", -- disable signcolumn
+      number = false, -- disable number column
+      relativenumber = false, -- disable relative numbers
       -- cursorline = false, -- disable cursorline
       -- cursorcolumn = false, -- disable cursor column
       -- foldcolumn = "0", -- disable fold column
@@ -26,17 +26,25 @@ require("zen-mode").setup {
       ruler = false, -- disables the ruler text in the cmd line area
       showcmd = false, -- disables the command in the last line of the screen
     },
-    gitsigns = { enabled = false }, -- disables git signs
+    gitsigns = { 
+      enabled = false 
+    }, -- disables git signs
     tmux = { enabled = false }, -- disables the tmux statusline
     -- this will change the font size on kitty when in zen mode
     -- to make this work, you need to set the following kitty options:
     -- - allow_remote_control socket-only
     -- - listen_on unix:/tmp/kitty
     kitty = {
-      enabled = false,
+      enabled = true,
       font = "+4", -- font size increment
     },
   },
+    -- callback where you can add custom code when the Zen window opens
+  on_open = function(win)
+    vim.cmd[[set wrap linebreak nolist]]
+  end,
+  -- callback where you can add custom code when the Zen window closes
+  on_close = function()
+    vim.cmd[[set nowrap nolinebreak list]]
+  end
 }
-
-vim.api.nvim_set_keymap('n', '<leader>z', ':ZenMode<CR>', { noremap = true, silent = true})
