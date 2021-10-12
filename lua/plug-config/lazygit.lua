316ea4104fd359fed2c668bdf4a30ce17092a852
nibru <nikolaus.brunner@protonmail.ch>
2021-10-12 21:17:27 +0200

    "Updates"

--- lua/plug-config/lazygit.lua
+++ lua/plug-config/lazygit.lua
@@ -1,5 +1,5 @@
 local options = {
-  lazygit_floating_window_winblend = 1,
+  lazygit_floating_window_winblend = 0,
   lazygit_floating_window_scaling_factor = 0.9,
   lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'},
   lazygit_use_neovim_remote = 1 
