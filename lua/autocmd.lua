85e9e0465bd5b3284d7f82585b43be03e49c6b80
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-10-14 23:16:43 +0200

    auto cmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,5 +1,7 @@
 -- set filetypes as typescriptreact
-vim.cmd "au BufNewFile,BufRead *.tsx,*.jsx set filetype=typescriptreact"
+vim.cmd "au BufNewFile,BufRead *jsx        set filetype=javascriptreact"
+vim.cmd "au BufNewFile,BufRead *.tsx       set filetype=typescriptreact"
+vim.cmd "au BufRead,BufNewFile *.js        set filetype=javascript"
 vim.cmd "au BufRead,BufNewFile *.ts        set filetype=typescript"
 
 -- Wrap lines for markdown files
