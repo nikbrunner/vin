3d0c6b8bf3ca85d8c3046a0d54347c64ee67d954
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-10-15 08:09:17 +0200

    Added opts

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -75,7 +75,7 @@ packer.startup(function()
     requires = {
       'L3MON4D3/LuaSnip',
       { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-      'hrsh7th/cmp-nvim-lsp',
+      { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
       { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
       { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
