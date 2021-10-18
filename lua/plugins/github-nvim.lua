35487b201460be4b3d53667db8c7540f1a912728
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-10-18 22:04:15 +0200

    Updated github-nvim

--- lua/plugins/github-nvim.lua
+++ lua/plugins/github-nvim.lua
@@ -2,7 +2,7 @@ require("github-theme").setup(
     {
       theme_style = "dark_default",
       hide_end_of_buffer = true,
-      dark_sidebar = true,
+      dark_sidebar = false,
       sidebars = { "qf", "vista_kind", "terminal", "packer", "lazygit" }
     }
 )
