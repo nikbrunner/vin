66026c43aead07f28beaf8f37dbd5624b7ffc459
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-10-18 11:38:34 +0200

    Adjusted WhichKeySingles gj,gk

--- lua/bindings/keymaps/singles.lua
+++ lua/bindings/keymaps/singles.lua
@@ -8,6 +8,9 @@ WhichKeySingles.noLeader = {
     ["H"] = { "^", "Line Start" },
     ["L"] = { "$", "Line End" },
 
+    ["k"] = { "gk", "Adjusted k" },
+    ["j"] = { "gj", "Adjusted j" },
+
     ["Y"] = { "y$", "Yank till end" },
     ["n"] = { "nzzzv", "Adjusted n" },
     ["N"] = { "Nzzzv", "Adjusted N" },
