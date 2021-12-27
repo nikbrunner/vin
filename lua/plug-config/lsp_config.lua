3c98410841e1f21ad63c6a0965dd27b47bae6b33
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-12-27 15:26:03 +0100

    Added comment with issue link for cssls

--- lua/plug-config/lsp_config.lua
+++ lua/plug-config/lsp_config.lua
@@ -1,6 +1,7 @@
 local nvim_lsp = require("lspconfig")
 
 -- TODO Fix cssls completion
+-- https://github.com/hrsh7th/cmp-nvim-lsp/issues/14
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
