34260b7581f9411f09a8dc9f0b61e43e93013277
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-06 14:28:00 +0100

    Replaced lunarvim/darkplus with lunarvim/colorsschemes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,8 +61,7 @@ return packer.startup(function(use)
 	use("ThePrimeagen/harpoon")
 
 	-- Colorschemes
-	-- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-	use("lunarvim/darkplus.nvim")
+	use("lunarvim/colorschemes")
 	use("projekt0n/github-nvim-theme")
 
 	-- cmp plugins
