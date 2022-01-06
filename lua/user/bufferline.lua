25f9e527e42de7be9c3b72d604d11b59f334ff03
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-06 13:28:49 +0100

    Added diagnostics to bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -34,7 +34,7 @@ bufferline.setup({
 		max_name_length = 30,
 		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
 		tab_size = 21,
-		diagnostics = false, -- | "nvim_lsp" | "coc",
+		diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
 		diagnostics_update_in_insert = false,
 		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
 		--   return "("..count..")"
