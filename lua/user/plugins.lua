b4f643c1b14998bc469a4eb7047015736818504f
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-06 13:26:59 +0100

    Added vim-autoread
    Updated plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,13 +51,13 @@ return packer.startup(function(use)
 	use("akinsho/bufferline.nvim")
 	use("moll/vim-bbye")
 	use("nvim-lualine/lualine.nvim")
-	-- use("akinsho/toggleterm.nvim")
 	use("ahmedkhalf/project.nvim")
 	use("lewis6991/impatient.nvim")
 	use("lukas-reineke/indent-blankline.nvim")
 	use("goolord/alpha-nvim")
 	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
 	use("folke/which-key.nvim")
+	use("djoshea/vim-autoread")
 
 	-- Colorschemes
 	-- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
@@ -95,7 +95,7 @@ return packer.startup(function(use)
 
 	-- Git
 	use("lewis6991/gitsigns.nvim")
-  use("kdheepak/lazygit.nvim")
+	use("kdheepak/lazygit.nvim")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
