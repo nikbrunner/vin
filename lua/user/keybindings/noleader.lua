607cb48ec13495620d32c7aaefb224c0e250085b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-06 12:06:52 +0100

    Added Q to nop

--- lua/user/keybindings/noleader.lua
+++ lua/user/keybindings/noleader.lua
@@ -10,6 +10,8 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap("n", "Q", "<Nop>", opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
