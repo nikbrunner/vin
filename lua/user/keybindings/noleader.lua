a16c98f3d84c37cae7b97ff7622392c4819e7ba4
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-07 19:40:50 +0100

    C-p for file navigation

--- lua/user/keybindings/noleader.lua
+++ lua/user/keybindings/noleader.lua
@@ -27,6 +27,14 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- VSCode like Control-p file navigation
+keymap(
+	"n",
+	"<C-p>",
+	"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+	opts
+)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
