e9b9d3d0bd78f2a15fbabbaf9eb0f6ffe1ab7a4a
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-08 16:46:53 +0100

    Added commands

---
+++ lua/user/whichkey/commands.lua
@@ -0,0 +1,30 @@
+local Commands = {}
+
+-- Function to copy path and filenames
+Commands.copy = {
+	fullPath = function()
+		return ":let @+ = expand('%:p')<CR>"
+	end,
+	relativePath = function()
+		return ":let @+ = expand('%')<CR>"
+	end,
+	fileName = function()
+		return ":let @+ = expand('%:t')<CR>"
+	end,
+}
+
+-- Function to toggle quickfix list
+Commands.toggleQuickFixList = function()
+	vim.cmd([[
+function! ToggleQuickFix()
+  if empty(filter(getwininfo(), 'v:val.quickfix'))
+    copen
+  else
+    cclose
+  endif
+endfunction
+]])
+end
+
+return Commands
+
