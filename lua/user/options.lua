79a3e3d65e40b15bc39456bd5dbd3d08103c8f6b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-09 00:22:41 +0100

    Updated options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,3 +44,6 @@ end
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
+
+-- hide vertical borders
+vim.cmd([[set fillchars+=vert:\ ]])
