f99afd43f847a67e8e76f623d7283a831fd8cac7
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-08 14:49:52 +0100

    Renamed whichkey file and changed some settings

--- lua/user/keybindings/whichkey.lua
+++ lua/user/keybindings/whichkey.lua
@@ -36,11 +36,11 @@ local setup = {
 		-- ["<cr>"] = "RET",
 		-- ["<tab>"] = "TAB",
 	},
-	icons = {
-		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-		separator = "➜", -- symbol used between a key and it's label
-		group = "+", -- symbol prepended to a group
-	},
+  icons = {
+    breadcrumb = "/", -- symbol used in the command line area that shows your active key combo
+    separator = "·", -- symbol used between a key and it's label
+    group = "" -- symbol prepended to a group
+  },
 	popup_mappings = {
 		scroll_down = "<c-d>", -- binding to scroll down inside the popup
 		scroll_up = "<c-u>", -- binding to scroll up inside the popup
@@ -53,11 +53,11 @@ local setup = {
 		winblend = 0,
 	},
 	layout = {
-		height = { min = 4, max = 25 }, -- min and max height of the columns
-		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
-		align = "left", -- align columns left, center or right
-	},
+    height = { min = 4, max = 50 }, -- min and max height of the columns
+    width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 5, -- spacing between columns,
+    align = "center"
+  },
 	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 	show_help = true, -- show help message on the command line when the popup is visible
