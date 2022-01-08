a06b3f7d4c5ec89c0d0aa530bab5d2722056c20a
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-08 16:11:12 +0100

    Modified keybindings/init.lua

--- lua/user/keybindings/init.lua
+++ lua/user/keybindings/init.lua
@@ -1,2 +1,27 @@
 require("user.keybindings.noleader")
-require("user.keybindings.whichkey")
+
+local mappingGroups = require("user.keybindings.mappings.groups")
+local wk_opts = require("user.keybindings.wk_opts");
+local wk_config = require("user.keybindings.wk_config")
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+	return
+end
+
+
+local mappings = {
+	[";"] = { "<cmd>Alpha<cr>", "Dashboard" },
+	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
+
+	P = mappingGroups.packer,
+	f = mappingGroups.find,
+	g = mappingGroups.git,
+	l = mappingGroups.lsp,
+  h = mappingGroups.harpoon,
+  i = mappingGroups.insert,
+  q = mappingGroups.quit
+}
+
+which_key.setup(wk_config)
+which_key.register(mappings, wk_opts.normal.withLeader)
