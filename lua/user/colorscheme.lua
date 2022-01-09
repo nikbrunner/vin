2d7356a3911f5d116c1d0ae48ff29c0fcae79bdf
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-09 11:23:01 +0100

    Formated colorscheme.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,14 +1,12 @@
-require("github-theme").setup(
-    {
-      theme_style = "dark_default",
-      hide_end_of_buffer = true,
-      dark_sidebar = true,
-      function_style = "italic",
-      keyword_style = "bold",
-      variable_style = "bold",
-      sidebars = { "qf", "vista_kind", "terminal", "packer", "lazygit" }
-    }
-)
+require("github-theme").setup({
+	theme_style = "dark_default",
+	hide_end_of_buffer = true,
+	dark_sidebar = true,
+	function_style = "italic",
+	keyword_style = "bold",
+	variable_style = "bold",
+	sidebars = { "qf", "vista_kind", "terminal", "packer", "lazygit" },
+})
 
 vim.cmd([[
 try
