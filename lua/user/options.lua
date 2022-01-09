dbda0e1b25711121b8af909805cbc9def2df8b89
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-09 12:04:52 +0100

    Fixed folds

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,6 +33,8 @@ local options = {
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
 	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	foldmethod = "indent",
+	foldlevel = 10,
 }
 
 vim.opt.shortmess:append("c")
