e572a925bd06d5e91ab80039bfea95133f31a9fa
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-09 10:48:26 +0100

    Enabled format on save

--- lua/user/utils/autocommands.lua
+++ lua/user/utils/autocommands.lua
@@ -28,10 +28,9 @@ vim.cmd([[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-]])
 
--- Autoformat
--- augroup _lsp
---   autocmd!
---   autocmd BufWritePre * lua vim.lsp.buf.formatting()
--- augroup end
+  augroup _lsp
+    autocmd!
+    autocmd BufWritePre * lua vim.lsp.buf.formatting()
+  augroup end
+]])
