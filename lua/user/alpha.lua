44aa9c4eee0a1204340c0e92c179306cf42a657c
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-12 19:48:05 +0100

    Updated alpha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -13,9 +13,9 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
@@ -23,7 +23,7 @@ dashboard.section.buttons.val = {
 }
 
 local function footer()
-	return "nikolaus.brunner@protonmail.ch"
+	return "https://nibru.dev"
 end
 
 dashboard.section.footer.val = footer()
