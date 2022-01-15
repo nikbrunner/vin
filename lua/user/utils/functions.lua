9ec945b211cd521a4ec8e34c44c833edabd027d5
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-15 14:10:21 +0100

    Added mapping function

--- lua/user/utils/functions.lua
+++ lua/user/utils/functions.lua
@@ -13,6 +13,14 @@ Functions.copy = {
 	end,
 }
 
+Functions.map = function(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 -- Function to toggle quickfix list
 vim.cmd([[
 function! ToggleQuickFix()
