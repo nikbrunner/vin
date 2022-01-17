b66c954d7140161e66107acc555f9ad3b8d0dc58
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-17 17:25:11 +0100

    Updated github_theme settings

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -7,7 +7,7 @@ github_theme.setup({
 	theme_style = "dark_default",
 	hide_end_of_buffer = true,
 	dark_sidebar = true,
-	function_style = "italic",
+	function_style = "bold",
 	keyword_style = "bold",
 	variable_style = "bold",
 	sidebars = { "qf", "vista_kind", "terminal", "packer", "lazygit" },
