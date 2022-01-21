23701e913f0677790104035be156205de326dc7d
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-21 12:19:31 +0100

    Disabled autoformat

--- lua/user/utils/autocommands.lua
+++ lua/user/utils/autocommands.lua
@@ -31,9 +31,9 @@ vim.cmd([[
 ]])
 
 -- Auto Formatting
-vim.cmd([[
-augroup _lsp
-  autocmd!
-  autocmd BufWritePre * lua vim.lsp.buf.formatting()
-augroup end
-]])
+-- vim.cmd([[
+-- augroup _lsp
+--   autocmd!
+--   autocmd BufWritePre * lua vim.lsp.buf.formatting()
+-- augroup end
+-- ]])
