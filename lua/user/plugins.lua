6352180af241700bb0e7932f095dd66c11470014
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-22 11:25:14 +0100

    Moved plugin configs to seperate folder

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -76,7 +76,12 @@ return packer.startup(function(use) -- My plugins here
 	use("projekt0n/github-nvim-theme")
 
 	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use({
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("user.configs.cmp")
+		end,
+	}) -- The completion plugin
 	use("hrsh7th/cmp-buffer") -- buffer completions
 	use("hrsh7th/cmp-path") -- path completions
 	use("hrsh7th/cmp-cmdline") -- cmdline completions
