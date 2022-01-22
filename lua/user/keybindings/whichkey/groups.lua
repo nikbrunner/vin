eefd351861c61f56f3aebdce6b78d2f2ca83ce0b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-22 10:01:30 +0100

    Resolved utils folder

--- lua/user/keybindings/whichkey/groups.lua
+++ lua/user/keybindings/whichkey/groups.lua
@@ -3,7 +3,7 @@ if not status_ok then
 	return
 end
 
-local functions = require("user.utils.functions")
+local functions = require("user.functions")
 local commands = require("user.commands")
 
 local WhichKeyGroups = {}
