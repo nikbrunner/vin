6a8e91960765946ee99188de4c44fe1083f7f419
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-22 17:52:07 +0100

    Ignore ocamllex due to buggyness

--- lua/vin/configs/treesitter.lua
+++ lua/vin/configs/treesitter.lua
@@ -6,7 +6,7 @@ end
 configs.setup({
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
+	ignore_install = { "ocamllex" }, -- List of parsers to ignore installing
 	autopairs = {
 		enable = true,
 	},
