8aa5798bcca6a3f298199bb70963aac2fba1bc26
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-22 11:34:06 +0100

    Introduced core/

---
+++ lua/user/core/init.lua
@@ -0,0 +1,4 @@
+require("user.core.options")
+require("user.core.autocommands")
+require("user.core.colorscheme")
+require("user.core.functions")
--- lua/user/keybindings/whichkey/groups.lua
+++ lua/user/keybindings/whichkey/groups.lua
@@ -3,7 +3,6 @@ if not status_ok then
 	return
 end
 
-local functions = require("user.functions")
 local commands = require("user.keybindings.commands")
 
 local WhichKeyGroups = {}
