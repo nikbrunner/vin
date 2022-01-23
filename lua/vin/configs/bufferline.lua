5f8899757059d24526fea611882f70150707d2af
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-23 21:55:04 +0100

    Updated bufferline icons

--- lua/vin/configs/bufferline.lua
+++ lua/vin/configs/bufferline.lua
@@ -64,8 +64,8 @@ bufferline.setup({
 		},
 		show_buffer_icons = true,
 		show_buffer_close_icons = true,
-		show_close_icon = true,
-		show_tab_indicators = false,
+		show_close_icon = false,
+		show_tab_indicators = true,
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		-- can also be a table containing 2 custom separators
 		-- [focused and unfocused]. eg: { '|', '|' }
