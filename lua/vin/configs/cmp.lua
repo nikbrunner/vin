c2b4c4630b6bde9d0b0123296b20d87feb13eb08
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-30 15:51:33 +0100

    Removed unused code in cmp.lua

--- lua/vin/configs/cmp.lua
+++ lua/vin/configs/cmp.lua
@@ -15,39 +15,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
---   פּ ﯟ   some other good icons
-local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 cmp.setup({
 	snippet = {
@@ -69,34 +36,6 @@ cmp.setup({
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		-- 	["<Tab>"] = cmp.mapping(function(fallback)
-		-- 		if cmp.visible() then
-		-- 			cmp.select_next_item()
-		-- 		elseif luasnip.expandable() then
-		-- 			luasnip.expand()
-		-- 		elseif luasnip.expand_or_jumpable() then
-		-- 			luasnip.expand_or_jump()
-		-- 		elseif check_backspace() then
-		-- 			fallback()
-		-- 		else
-		-- 			fallback()
-		-- 		end
-		-- 	end, {
-		-- 		"i",
-		-- 		"s",
-		-- 	}),
-		-- 	["<S-Tab>"] = cmp.mapping(function(fallback)
-		-- 		if cmp.visible() then
-		-- 			cmp.select_prev_item()
-		-- 		elseif luasnip.jumpable(-1) then
-		-- 			luasnip.jump(-1)
-		-- 		else
-		-- 			fallback()
-		-- 		end
-		-- 	end, {
-		-- 		"i",
-		-- 		"s",
-		-- 	}),
 	},
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
