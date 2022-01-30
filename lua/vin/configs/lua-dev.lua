1335f4cef997d47e9f6ddb2f2570a7c48e773f47
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-30 15:28:54 +0100

    Updated lua-dev

--- lua/vin/configs/lua-dev.lua
+++ lua/vin/configs/lua-dev.lua
@@ -1,9 +1,15 @@
-local luadev = require("lua-dev").setup({
+local status_ok, luadev = pcall(require, "lua-dev")
+if not status_ok then
+	return
+end
+
+luadev.setup({
 	-- add any options here, or leave empty to use the default settings
 	-- lspconfig = {
-	--   cmd = {"lua-language-server"}
+	-- 	cmd = { "lua-language-server" },
 	-- },
 })
 
 local lspconfig = require("lspconfig")
+
 lspconfig.sumneko_lua.setup(luadev)
