322ae795f7c91e446be24ff1e4d187090542d960
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-01-30 08:38:23 +0100

    Removed transparent background for gh

--- lua/vin/core/colorscheme/github.lua
+++ lua/vin/core/colorscheme/github.lua
@@ -7,7 +7,7 @@ github_theme.setup({
 	theme_style = "dark_default",
 	hide_end_of_buffer = true,
   hide_inactive_statusline = false,
-  transparent = true,
+  transparent = false,
   dark_float = true,
 	dark_sidebar = true,
 	function_style = "bold",
