42a99c967e71a3cdbf1584a86b4b8d6354909ef5
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-03-13 18:59:04 +0100

    Changed telescope settings

--- lua/vin/configs/telescope.lua
+++ lua/vin/configs/telescope.lua
@@ -8,9 +8,33 @@ local actions = require("telescope.actions")
 telescope.setup({
 	defaults = {
 
-		prompt_prefix = " ",
-		selection_caret = " ",
+		-- prompt_prefix = " ",
+		-- selection_caret = " ",
 		path_display = { "smart" },
+
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
+		winblend = 0,
+		border = {},
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 
 		mappings = {
 			i = {
