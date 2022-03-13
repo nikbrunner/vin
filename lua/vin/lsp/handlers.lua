b96c5d8e05021be87c30a6ba042fb053a4397f14
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-03-13 19:04:38 +0100

    :Format

--- lua/vin/lsp/handlers.lua
+++ lua/vin/lsp/handlers.lua
@@ -66,17 +66,16 @@ local function lsp_highlight_document(client)
 	end
 end
 
--- Not used anymore, but i  want to keep it for reference
--- local function lsp_keymaps(bufnr)
--- 	local opts = { noremap = true, silent = true }
--- 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
--- end
+local function lsp_keymaps(bufnr)
+	-- local opts = { noremap = true, silent = true }
+	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
 	end
-	-- lsp_keymaps(bufnr)
+	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
 end
 
