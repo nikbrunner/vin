1965928a9890ee74e6b0db5003ab118c74d94dc4
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-03-26 13:12:26 +0100

    Formating use() rules

--- lua/vin/plugins.lua
+++ lua/vin/plugins.lua
@@ -49,36 +49,36 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use) -- My plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
 
 	-- Libs for Utility and general Improvements
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use("lewis6991/impatient.nvim")
+	use({ "nvim-lua/popup.nvim" }) -- An implementation of the Popup API from vim in Neovim
+	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used ny lots of plugins
+	use({ "lewis6991/impatient.nvim" })
 
 	-- UI Enhancements
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("akinsho/bufferline.nvim")
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "kyazdani42/nvim-tree.lua" })
+	use({ "akinsho/bufferline.nvim" })
 	use({ "windwp/windline.nvim" })
-	use("goolord/alpha-nvim")
-	use("folke/which-key.nvim")
+	use({ "goolord/alpha-nvim" })
+	use({ "folke/which-key.nvim" })
 	use({ "stevearc/dressing.nvim" })
 
 	-- Workflow
-	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("numToStr/Comment.nvim") -- Easily comment stuff
-	use("lukas-reineke/indent-blankline.nvim") -- Indent Outline
+	use({ "antoinemadec/FixCursorHold.nvim" }) -- This is needed to fix lsp doc highlight
+	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "numToStr/Comment.nvim" }) -- Easily comment stuff
+	use({ "lukas-reineke/indent-blankline.nvim" }) -- Indent Outline
 	use({ "numToStr/BufOnly.nvim", cmd = "BufOnly" })
-	use("moll/vim-bbye")
+	use({ "moll/vim-bbye" })
 	use({ "RRethy/vim-illuminate" })
 	use({ "tpope/vim-surround" })
 	use({ "wellle/targets.vim" })
 	use({ "mg979/vim-visual-multi", branch = "master" })
-	use("djoshea/vim-autoread")
-	use("ThePrimeagen/harpoon")
-	use("gennaro-tedesco/nvim-jqx")
+	use({ "djoshea/vim-autoread" })
+	use({ "ThePrimeagen/harpoon" })
+	use({ "gennaro-tedesco/nvim-jqx" })
 	use({ "weilbith/nvim-code-action-menu", cmd = "CodeActionMenu" })
 	use({ "github/copilot.vim", disable = true })
 	use({ "kevinhwang91/nvim-bqf", ft = "qf" })
@@ -98,30 +98,30 @@ return packer.startup(function(use) -- My plugins here
 
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("hrsh7th/cmp-nvim-lsp")
+	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
+	use({ "hrsh7th/cmp-path" }) -- path completions
+	use({ "hrsh7th/cmp-cmdline" }) -- cmdline completions
+	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
+	use({ "hrsh7th/cmp-nvim-lsp" })
 
 	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+	use({ "L3MON4D3/LuaSnip" }) --snippet engine
+	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
 	-- LSP
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
-	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-	use("ray-x/lsp_signature.nvim") --for parameter hints
+	use({ "neovim/nvim-lspconfig" }) -- enable LSP
+	use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
+	use({ "tamago324/nlsp-settings.nvim" }) -- language server settings defined in json for
+	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+	use({ "ray-x/lsp_signature.nvim" }) --for parameter hints
 	use({ "j-hui/fidget.nvim" }) -- for LSP progress
-	use("folke/lua-dev.nvim")
+	use({ "folke/lua-dev.nvim" })
 	use({ "onsails/lspkind-nvim" })
 	use({ "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
 	-- Fuzzy finding
-	use("nvim-telescope/telescope.nvim")
-	use("ahmedkhalf/project.nvim")
+	use({ "nvim-telescope/telescope.nvim" })
+	use({ "ahmedkhalf/project.nvim" })
 	use({ "nvim-telescope/telescope-github.nvim" })
 	use({ "junegunn/fzf", run = "./install --bin" })
 	use({ "ibhagwan/fzf-lua" })
@@ -131,11 +131,11 @@ return packer.startup(function(use) -- My plugins here
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
 
 	-- Git
-	use("lewis6991/gitsigns.nvim")
-	use("kdheepak/lazygit.nvim")
+	use({ "lewis6991/gitsigns.nvim" })
+	use({ "kdheepak/lazygit.nvim" })
 	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
 	use({ "pwntester/octo.nvim" })
 
