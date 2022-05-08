09897887e53b942e18e785ef99e9981271ab8163
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-08 15:26:38 +0200

    enabled virtual text

--- lua/vin/lsp/handlers.lua
+++ lua/vin/lsp/handlers.lua
@@ -17,8 +17,7 @@ M.setup = function()
   end
 
   local config = {
-    -- disable virtual text
-    virtual_text = false,
+    virtual_text = true,
     -- show signs
     signs = {
       active = signs,
