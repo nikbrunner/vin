69a6e5f3c3145173291f1367e86262b532bd0847
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-10 22:00:15 +0200

    Custom LSP Rename function

--- lua/vin/core/commands/lsp.lua
+++ lua/vin/core/commands/lsp.lua
@@ -28,4 +28,70 @@ Lsp.code_action = function()
 	})
 end
 
+-- returns a table containing the lsp changes counts from an lsp result
+local function count_lsp_res_changes(lsp_res)
+	local count = { instances = 0, files = 0 }
+	if lsp_res.documentChanges then
+		for _, changed_file in pairs(lsp_res.documentChanges) do
+			count.files = count.files + 1
+			count.instances = count.instances + #changed_file.edits
+		end
+	elseif lsp_res.changes then
+		for _, changed_file in pairs(lsp_res.changes) do
+			count.instances = count.instances + #changed_file
+			count.files = count.files + 1
+		end
+	end
+	return count
+end
+
+Lsp.symbol_rename = function()
+	local curr_name = vim.fn.expand("<cword>")
+	local input_opts = {
+		prompt = "New name?",
+		default = curr_name,
+	}
+	-- ask user input
+	vim.ui.input(input_opts, function(new_name)
+		-- check new_name is valid
+		if not new_name or #new_name == 0 or curr_name == new_name then
+			return
+		end
+
+		-- request lsp rename
+		local params = vim.lsp.util.make_position_params()
+		params.newName = new_name
+
+		vim.lsp.buf_request(
+			0,
+			"textDocument/rename",
+			params,
+			function(_, res, ctx, _)
+				if not res then
+					return
+				end
+
+				-- apply renames
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_workspace_edit(res, client.offset_encoding)
+
+				-- display a message
+				local changes = count_lsp_res_changes(res)
+				local message = string.format(
+					"Renamed %s instance%s in %s file%s. %s",
+					changes.instances,
+					changes.instances == 1 and "" or "s",
+					changes.files,
+					changes.files == 1 and "" or "s",
+					changes.files > 1 and "To save them run ':wa'!" or ""
+				)
+
+				notify(message)
+			end
+		)
+	end)
+end
+
+vim.api.nvim_create_user_command("VinLspRename", Lsp.symbol_rename, {})
+
 return Lsp
