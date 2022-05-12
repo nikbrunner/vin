3c2448885ee681fbd38272a20369eddc00320dd8
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-12 19:13:52 +0200

    load function for init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
-require("vin.core")
-require("vin.plugins")
-require("vin.configs")
-require("vin.lsp")
-require("vin.keybindings")
+-- Function to savely load modules when sourcing the the init.lua
+local function load(mod)
+  package.loaded[mod] = nil
+  return require(mod)
+end
+
+load("vin.core")
+load("vin.plugins")
+load("vin.configs")
+load("vin.lsp")
+load("vin.keybindings")
