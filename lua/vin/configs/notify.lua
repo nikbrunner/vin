18e749356cb5e109dbadfb78bac5f222a8d76c8f
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-19 00:23:04 +0200

    Updated colors for notify

--- lua/vin/configs/notify.lua
+++ lua/vin/configs/notify.lua
@@ -26,7 +26,7 @@ notify.setup({
 
 	-- For stages that change opacity this is treated as the highlight behind the window
 	-- Set this to either a highlight group, an RGB hex value e.g. "#000000" or a function returning an RGB code for dynamic values
-	background_colour = "Normal",
+	background_colour = "NormalFloat",
 
 	-- Minimum width for notification windows
 	minimum_width = 50,
