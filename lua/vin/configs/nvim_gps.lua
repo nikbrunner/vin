96ddbc3ff492fd8412d2a116417edb463241bad5
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-20 20:40:04 +0200

    Updated gps config

--- lua/vin/configs/nvim_gps.lua
+++ lua/vin/configs/nvim_gps.lua
@@ -12,8 +12,8 @@ gps.setup({
 		["class-name"] = " ", -- Classes and class-like objects
 		["function-name"] = " ", -- Functions
 		["method-name"] = " ", -- Methods (functions inside class-like objects)
-		["container-name"] = "⛶ ", -- Containers (example: lua tables)
-		["tag-name"] = "炙", -- Tags (example: html tags)
+		["container-name"] = " ", -- Containers (example: lua tables)
+		["tag-name"] = " ", -- Tags (example: html tags)
 	},
 
 	-- Add custom configuration per language or
