b73bb797e586630758c3c96bfe69c228f6b7d45a
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-21 08:59:56 +0200

    Updated colorscheme initalization

--- lua/vin/core/colorscheme/init.lua
+++ lua/vin/core/colorscheme/init.lua
@@ -4,15 +4,23 @@ require("vin.core.colorscheme.onenord")
 require("vin.core.colorscheme.vscode")
 require("vin.core.colorscheme.kanagawa")
 
-vim.cmd([[
-try
-  colorscheme onenord
-  catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]])
+local colorscheme = "onenord"
 
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+
+	vim.cmd([[
+    catch /^Vim\%((\a\+)\)\=:E185/
+    colorscheme default
+    set background=dark
+  ]])
+
+	return
+end
+
+-- Custom Highlights
 vim.cmd([[
   hi StatusLine gui=NONE guifg=NONE guibg=NonText guisp=NonText
   hi StatusLineNc gui=NONE guifg=NONE guibg=NonText guisp=NonText
