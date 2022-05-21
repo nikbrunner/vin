8572c92c0a93344eedfc23685f6fa9bde018673e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-21 11:51:09 +0200

    Introdoce find_scss_symbol

--- lua/vin/core/commands/telescope.lua
+++ lua/vin/core/commands/telescope.lua
@@ -11,7 +11,19 @@ if not telescope_status_ok then
 	return
 end
 
+local notify_status_ok, notify = pcall(require, "notify")
+if not notify_status_ok then
+	return
+end
+
 local general_commands = require("vin.core.commands.general")
+
+local telescope_notification = function(message)
+	notify(message, "info", {
+		title = "Telescope",
+		icon = "",
+	})
+end
 
 local M = {}
 
@@ -22,20 +34,39 @@ M.find_related_files = function()
 	})
 end
 
-M.find_mixin = function()
-	local current_word = vim.fn.expand("<cword>")
+M.find_scss_symbol = function()
+	local curr_word = vim.fn.expand("<cword>")
 
-	telescope_builtins.grep_string({
-		default_text = "@mixin " .. current_word,
-	})
-end
+	local prompt = "What are you looking for?"
 
-M.find_scss_variable = function()
-	local curr_name = vim.fn.expand("<cword>")
+	local symbol_types = { "variable", "mixin" }
 
-	telescope_builtins.grep_string({
-		default_text = "$" .. curr_name .. ": ",
-	})
+	local handleFormat = function(item)
+		if item == symbol_types[1] then
+			return "$" .. item
+		elseif item == symbol_types[2] then
+			return "@" .. item
+		end
+	end
+
+	local handleSelection = function(symbol_type)
+		if symbol_type == symbol_types[1] then
+			local query = "$" .. curr_word .. ": "
+
+			telescope_builtins.grep_string({ default_text = query })
+			telescope_notification("Looking for '$" .. curr_word .. "'")
+		elseif symbol_type == symbol_types[2] then
+			local query = "@mixin " .. curr_word
+
+			telescope_builtins.grep_string({ default_text = query })
+			telescope_notification("Looking for '@" .. curr_word .. "'")
+		end
+	end
+
+	vim.ui.select(symbol_types, {
+		prompt = prompt,
+		format_item = handleFormat,
+	}, handleSelection)
 end
 
 -- Find project folders with telescope extension
