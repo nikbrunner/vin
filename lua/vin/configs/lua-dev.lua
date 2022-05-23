7393830b97531201dcdeb3af1ed5865330627b9e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-23 22:16:10 +0200

    Updated lua dev config

--- lua/vin/configs/lua-dev.lua
+++ lua/vin/configs/lua-dev.lua
@@ -4,10 +4,19 @@ if not status_ok then
 end
 
 luadev.setup({
-	-- add any options here, or leave empty to use the default settings
-	-- lspconfig = {
-	-- 	cmd = { "lua-language-server" },
-	-- },
+	library = {
+		vimruntime = true, -- runtime path
+		types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+		plugins = true, -- installed opt or start plugins in packpath
+		-- you can also specify the list of plugins to make available as a workspace library
+		-- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+	},
+	runtime_path = false, -- enable this to get completion in require strings. Slow!
+	-- pass any additional options that will be merged in the final lsp config
+	lspconfig = {
+		cmd = { "lua-language-server" },
+		-- on_attach = ...
+	},
 })
 
 local lspconfig = require("lspconfig")
