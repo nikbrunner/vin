b4fcc9b6bdae46f09fe1c5f2c31a4c71354e6342
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-23 21:24:20 +0200

    Removed bufferline from plugins
    no need because of tabby

--- lua/vin/plugins.lua
+++ lua/vin/plugins.lua
@@ -68,11 +68,6 @@ return packer.startup(function(use) -- My plugins here
 		},
 	})
 	use({
-		"akinsho/bufferline.nvim",
-		tag = "v2.*",
-		requires = "kyazdani42/nvim-web-devicons",
-	})
-	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
