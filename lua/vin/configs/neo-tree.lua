de96e60c92ec261c2683632ae59c1014b5f254ab
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-26 13:54:01 +0200

    neotree: indent icons and padding

--- lua/vin/configs/neo-tree.lua
+++ lua/vin/configs/neo-tree.lua
@@ -27,11 +27,13 @@ neotree.setup({
 	default_component_configs = {
 		indent = {
 			indent_size = 2,
-			padding = 1, -- extra padding on left hand side
+			padding = 2, -- extra padding on left hand side
 			-- indent guides
 			with_markers = true,
-			indent_marker = "│",
-			last_indent_marker = "└",
+			-- indent_marker = "│",
+			indent_marker = "·",
+			-- last_indent_marker = "└",
+			last_indent_marker = "·",
 			highlight = "NeoTreeIndentMarker",
 			-- expander config, needed for nesting files
 			with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
