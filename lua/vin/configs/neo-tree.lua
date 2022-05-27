6d0c8e5a1b82b78cbfbda2243db529498db33c42
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-05-27 09:00:52 +0200

    neotree: removed print message

--- lua/vin/configs/neo-tree.lua
+++ lua/vin/configs/neo-tree.lua
@@ -217,8 +217,6 @@ neotree.setup({
 	},
 })
 
-print("hello world")
-
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
