f209cc71b2fb0c8b123d357d4ebdbc6f73ce35ea
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-06-04 08:39:39 +0200

    colorscheme: added comment

--- lua/vin/core/colorscheme/init.lua
+++ lua/vin/core/colorscheme/init.lua
@@ -6,6 +6,8 @@ require("vin.core.colorscheme.kanagawa")
 
 local colorscheme = "onenord"
 
+-- TODO Custom Toggle to Switch to a light theme
+
 local notify_status_ok, notify = pcall(require, "notify")
 if not notify_status_ok then
 	return
