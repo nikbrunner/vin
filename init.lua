a24bcd8c0d5c4cc540a4c04429ecb27869d38f43
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-06-05 17:52:34 +0200

    vin: introduce lua/vin/init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
--- Function to savely load modules when sourcing the the init.lua
-local function load(mod)
-	package.loaded[mod] = nil
-	return require(mod)
-end
-
-load("vin.core")
-load("vin.plugins")
-load("vin.configs")
-load("vin.lsp")
-load("vin.keybindings")
+require("vin")
