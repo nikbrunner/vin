92ef7ec19961f33640d57bb0c41c6bbeef0cfc20
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-06-26 09:20:02 +0200

    bufferline: remove groups

--- lua/vin/plug-configs/bufferline.lua
+++ lua/vin/plug-configs/bufferline.lua
@@ -41,34 +41,6 @@ bufferline.setup({
 			local icon = level:match("error") and " " or " "
 			return " " .. icon .. count
 		end,
-		-- TODO: These do not work
-		-- groups = {
-		-- 	options = {
-		-- 		toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-		-- 	},
-		-- 	items = {
-		-- 		{
-		-- 			name = "Tests", -- Mandatory
-		-- 			highlight = { gui = "underline", guisp = "blue" }, -- Optional
-		-- 			priority = 2, -- determines where it will appear relative to other groups (Optional)
-		-- 			icon = "", -- Optional
-		-- 			matcher = function(buf) -- Mandatory
-		-- 				return buf.name:match("%_test") or buf.name:match("%_spec")
-		-- 			end,
-		-- 		},
-		-- 		{
-		-- 			name = "Docs",
-		-- 			highlight = { gui = "undercurl", guisp = "green" },
-		-- 			auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-		-- 			matcher = function(buf)
-		-- 				return buf.name:match("%.md") or buf.name:match("%.txt")
-		-- 			end,
-		-- 			separator = { -- Optional
-		-- 				style = require("bufferline.groups").separator.tab,
-		-- 			},
-		-- 		},
-		-- 	},
-		-- },
 		-- NOTE: this will be called a lot so don't do any heavy processing here
 		-- custom_filter = function(buf_number, buf_numbers)
 		-- 	-- filter out filetypes you don't want to see
