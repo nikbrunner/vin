9d04908fc84112ee3e4b3987ae6ef977f1dfca7b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-11 17:54:23 +0200

    nvimtree: custom map for next/prev change

--- lua/vin/plug-configs/nvim-tree.lua
+++ lua/vin/plug-configs/nvim-tree.lua
@@ -123,6 +123,8 @@ nvim_tree.setup({
 				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
 				{ key = "h", cb = tree_cb("close_node") },
 				{ key = "v", cb = tree_cb("vsplit") },
+				{ key = "[g", action = "prev_git_item" },
+				{ key = "]g", action = "next_git_item" },
 			},
 		},
 		number = false,
