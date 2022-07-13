bfa52f61f9ce126cb697acb29cdeebbd560773ce
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-13 18:50:40 +0200

    trouble: reduce height

--- lua/vin/plug-configs/trouble.lua
+++ lua/vin/plug-configs/trouble.lua
@@ -5,7 +5,7 @@ end
 
 trouble.setup({
 	position = "bottom", -- position of the list can be: bottom, top, left, right
-	height = 10, -- height of the trouble list when position is top or bottom
+	height = 5, -- height of the trouble list when position is top or bottom
 	width = 50, -- width of the list when position is left or right
 	icons = true, -- use devicons for filenames
 	mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
