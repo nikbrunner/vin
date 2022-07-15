45fe2c0cf2cfec41400b4efd98f4d2e73798e300
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-15 14:07:41 +0200

    trouble: update height and auto_close

--- lua/vin/plug-configs/trouble.lua
+++ lua/vin/plug-configs/trouble.lua
@@ -5,7 +5,7 @@ end
 
 trouble.setup({
 	position = "bottom", -- position of the list can be: bottom, top, left, right
-	height = 5, -- height of the trouble list when position is top or bottom
+	height = 10, -- height of the trouble list when position is top or bottom
 	width = 50, -- width of the list when position is left or right
 	icons = true, -- use devicons for filenames
 	mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
@@ -36,7 +36,7 @@ trouble.setup({
 	},
 	indent_lines = true, -- add an indent guide below the fold icons
 	auto_open = false, -- automatically open the list when you have diagnostics
-	auto_close = false, -- automatically close the list when you have no diagnostics
+	auto_close = true, -- automatically close the list when you have no diagnostics
 	auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
 	auto_fold = false, -- automatically fold a file trouble list at creation
 	auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
