f19e6eb7ef7b375a77ac40278361242859302115
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-23 11:27:40 +0200

    telescope: path truncate

--- lua/vin/plug-configs/telescope.lua
+++ lua/vin/plug-configs/telescope.lua
@@ -10,7 +10,7 @@ telescope.setup({
 
 		-- prompt_prefix = " ",
 		-- selection_caret = " ",
-		path_display = { "smart" },
+		path_display = { "truncate" },
 
 		prompt_prefix = "   ",
 		selection_caret = "  ",
