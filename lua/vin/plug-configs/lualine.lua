f5023664cf950457b89484c2f8a92115cde3cf28
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-23 09:23:35 +0200

    lualine: separators

--- lua/vin/plug-configs/lualine.lua
+++ lua/vin/plug-configs/lualine.lua
@@ -27,11 +27,11 @@ lualine.setup({
 		-- section_separators = { left = "", right = "" },
 		-- component_separators = { left = "", right = "" },
 
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
 
-		-- section_separators = { left = "🭲", right = "🭲" },
-		-- component_separators = { left = "🭰", right = "🭵" },
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
 
 		disabled_filetypes = {
 			"alpha",
