3684c168c947539e89813d43764b0d260582fef2
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-23 11:27:30 +0200

    lualine: update config and added winbar

--- lua/vin/plug-configs/lualine.lua
+++ lua/vin/plug-configs/lualine.lua
@@ -8,95 +8,117 @@ if not gps_status_ok then
 	return
 end
 
-local lualine_theme
-local current_colorscheme = Vin.lib.get_current_colorscheme()
-
-if current_colorscheme == "material" then
-	lualine_theme = "material-stealth"
-else
-	lualine_theme = "auto"
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
 end
+
+local mode = {
+	"mode",
+	padding = 3,
+}
+
+local branch = {
+	"branch",
+	padding = 3,
+}
+
+local diff = {
+	"diff",
+	colored = true, -- Displays a colored diff status if set to true
+	diff_color = {
+		-- Same color values as the general color option can be used here.
+		added = "DiffAdd", -- Changes the diff's added color
+		modified = "DiffChange", -- Changes the diff's modified color
+		removed = "DiffDelete", -- Changes the diff's removed color you
+	},
+	symbols = { added = "+", modified = "~", removed = "-" }, -- Changes the symbols used by the diff.
+	cond = hide_in_width,
+}
+
+local filetype = {
+	"filetype",
+	colored = true, -- Displays filetype icon in color if set to true
+	icon_only = false, -- Display only an icon for filetype
+	icon = { align = "left" }, -- Display filetype icon on the right hand side
+	-- icon =    {'X', align='right'}
+	-- Icon string ^ in table is ignored in filetype component
+	padding = 3,
+}
+
+local filename = {
+	"filename",
+	file_status = true, -- Displays file status (readonly status, modified status)
+	path = 1, -- 0: Just the filename
+	-- 1: Relative path
+	-- 2: Absolute path
+	-- 3: Absolute path, with tilde as the home directory
+
+	shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+	-- for other components. (terrible name, any suggestions?)
+	symbols = {
+		modified = "", -- Text to show when the file is modified.
+		readonly = "", -- Text to show when the file is non-modifiable or readonly.
+		unnamed = "[No Name]", -- Text to show for unnamed buffers.
+	},
+}
 
 lualine.setup({
 	options = {
+		globalstatus = true,
 		icons_enabled = true,
-		theme = lualine_theme,
+		theme = "auto",
 		-- component_separators = { left = "", right = "" },
 		-- section_separators = { left = "", right = "" },
-
 		-- section_separators = { left = "", right = "" },
 		-- component_separators = { left = "", right = "" },
-
 		-- section_separators = { left = "", right = "" },
 		-- component_separators = { left = "", right = "" },
-
 		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
-
 		disabled_filetypes = {
-			"alpha",
-			-- "neo-tree",
-			-- "",
+			statusline = {
+				"alpha",
+				"neo-tree",
+				"NvimTree",
+			},
+			winbar = {
+				"alpha",
+				"neo-tree",
+				"NvimTree",
+			},
 		},
 		always_divide_middle = true,
-		globalstatus = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
 	},
 	sections = {
-		lualine_a = {
-			{
-				"mode",
-				padding = 3,
-			},
-		},
-		lualine_b = {
-			{
-				"branch",
-				padding = 3,
-			},
-			"diff",
-			"diagnostics",
-		},
-		lualine_c = {
-			{
-				"filename",
-				file_status = true, -- Displays file status (readonly status, modified status)
-				path = 0, -- 0: Just the filename
-				-- 1: Relative path
-				-- 2: Absolute path
-				-- 3: Absolute path, with tilde as the home directory
-
-				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-				-- for other components. (terrible name, any suggestions?)
-				symbols = {
-					modified = "[+]", -- Text to show when the file is modified.
-					readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-					unnamed = "[No Name]", -- Text to show for unnamed buffers.
-				},
-			},
-			{ gps.get_location, cond = gps.is_available },
-		},
+		lualine_a = { mode },
+		lualine_b = { branch },
+		lualine_c = { filename, diff, "diagnostics" },
 		lualine_x = {},
-		lualine_y = {
-			{
-				"filetype",
-				colored = true, -- Displays filetype icon in color if set to true
-				icon_only = false, -- Display only an icon for filetype
-				icon = { align = "left" }, -- Display filetype icon on the right hand side
-				-- icon =    {'X', align='right'}
-				-- Icon string ^ in table is ignored in filetype component
-				padding = 3,
-			},
-		},
+		lualine_y = { filetype },
 		lualine_z = {},
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
+		lualine_c = {},
+		lualine_x = {},
 		lualine_y = {},
 		lualine_z = {},
 	},
 	tabline = {},
 	extensions = {},
+	winbar = {
+		lualine_c = { filename },
+		lualine_x = {
+			{ gps.get_location, cond = gps.is_available },
+		},
+	},
+	inactive_winbar = {
+		lualine_c = { filename },
+	},
 })
