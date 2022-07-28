e4c471350016dc3c3fd749089d489ec567c10561
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-28 17:52:53 +0200

    lualine: switch seperators

--- lua/vin/plug-configs/lualine.lua
+++ lua/vin/plug-configs/lualine.lua
@@ -71,10 +71,10 @@ lualine.setup({
 		-- section_separators = { left = "", right = "" },
 		-- section_separators = { left = "", right = "" },
 		-- component_separators = { left = "", right = "" },
-		-- section_separators = { left = "", right = "" },
-		-- component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {
 				"alpha",
