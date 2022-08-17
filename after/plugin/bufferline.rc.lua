df6f88af5cc6cecc6d0c9aae49443f865d69062e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-08-17 06:59:40 +0200

    bufferline: update
    squash bufferline

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -41,32 +41,12 @@ bufferline.setup({
             return " " .. icon .. count
         end,
         -- NOTE: this will be called a lot so don't do any heavy processing here
-        -- custom_filter = function(buf_number, buf_numbers)
-        -- 	-- filter out filetypes you don't want to see
-        -- 	if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        -- 		return true
-        -- 	end
-        --
-        -- 	if vim.bo[buf_number].filetype ~= "" then
-        -- 		return true
-        -- 	end
-        --
-        -- 	-- filter out by buffer name
-        -- 	if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        -- 		return true
-        -- 	end
-        -- 	-- filter out based on arbitrary rules
-        -- 	-- e.g. filter out vim wiki buffer from tabline in your work repo
-        -- 	if
-        -- 		vim.fn.getcwd() == "<work-repo>"
-        -- 		and vim.bo[buf_number].filetype ~= "wiki"
-        -- 	then
-        -- 		return true
-        -- 	end
-        -- 	-- filter out by it's index number in list (don't show first buffer)
-        -- 	if buf_numbers[1] ~= buf_number then
-        -- 		return true
-        -- 	end
+        -- custom_filter = function(buf, buf_nums)
+        -- if vim.bo[buf].filetype == "NvimTree" then
+        --     return false
+        -- else
+        --     return true
+        -- end
         -- end,
         offsets = {
             {
@@ -92,6 +72,6 @@ bufferline.setup({
         separator_style = "thick", -- "slant" | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        sort_by = "relative_directory",
+        sort_by = "tabs",
     },
 })
