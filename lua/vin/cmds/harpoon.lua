fa72d23845cb83ce51d8e685e42ae41357534aed
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-10-03 12:21:09 +0200

    harpoon: remove notifications

--- lua/vin/cmds/harpoon.lua
+++ lua/vin/cmds/harpoon.lua
@@ -1,10 +1,5 @@
-local notify_status_ok, notify = pcall(require, "notify")
-if not notify_status_ok then
-    return
-end
-
-local harpoon_status_ok, _ = pcall(require, "harpoon")
-if not harpoon_status_ok then
+local present, _ = pcall(require, "harpoon")
+if not present then
     return
 end
 
@@ -29,12 +24,6 @@ Vin.cmds.harpoon.add_file = function()
     -- require("neo-tree.sources.manager").refresh()
 
     local filename = vim.fn.expand("%:t")
-
-    notify("Added '" .. filename .. "' to Harpoooon!", "info", {
-        title = title,
-        timeout = 3000,
-        icon = icons.marked,
-    })
 end
 
 Vin.cmds.harpoon.jump_to_file = function(index)
@@ -43,10 +32,4 @@ end
 
 Vin.cmds.harpoon.toggle_quick_menu = function()
     ui.toggle_quick_menu()
-
-    notify("Pick or edit these files!", "info", {
-        title = title,
-        timeout = 3000,
-        icon = icons.success,
-    })
 end
