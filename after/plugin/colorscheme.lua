017298e4dedf13de15b77f981ce32d6f0d86dda6
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-12-25 13:00:50 +0100

    colorscheme: update file

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,21 +1,20 @@
-local colorscheme = "terra"
-
 local terra_present, terra = pcall(require, "terra")
-if not terra_present then
-    return
-else
+if terra_present then
     terra.setup({
         season = "summer",
     })
 end
 
-function ColorMyPencils(color)
-    color = color or colorscheme
-    vim.cmd.colorscheme(color)
-
+---@diagnostic disable-next-line: unused-function
+local transparentBackground = function()
     -- Enable these to make the backgrounds transparent
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+function PaintMe(color)
+    color = color or "default"
+    vim.cmd.colorscheme(color)
+end
+
+PaintMe("terra")
