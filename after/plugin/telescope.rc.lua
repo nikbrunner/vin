dcdb292151b50139dae27dadbda6914a52a3e8f2
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-12-28 09:04:39 +0100

    telescope: layout_strategy "flex"

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -15,7 +15,7 @@ telescope.setup({
         initial_mode = "insert",
         selection_strategy = "reset",
         sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
+        layout_strategy = "flex",
         layout_config = {
             horizontal = {
                 preview_width = 0.65,
