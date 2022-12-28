34ffb0554a3944796b32f450bd0d2449c0e36e7b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-12-28 09:04:53 +0100

    zen: window.backdrop 1

--- after/plugin/zen.rc.lua
+++ after/plugin/zen.rc.lua
@@ -5,7 +5,7 @@ end
 
 zen_mode.setup({
     window = {
-        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
         -- height and width can be:
         -- * an absolute number of cells when > 1
         -- * a percentage of the width / height of the editor when <= 1
