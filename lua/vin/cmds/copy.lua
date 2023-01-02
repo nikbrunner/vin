7004ac594d385351fda4b3e5f58f7c7f521f6647
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-02 18:54:39 +0100

    copy: setup var before copy

--- lua/vin/cmds/copy.lua
+++ lua/vin/cmds/copy.lua
@@ -7,16 +7,19 @@ end
 local M = {}
 
 M.fullPath = function()
-    copy(vim.fn.expand("%:p"))
+    local full_path = vim.fn.expand("%:p")
+    copy(full_path)
 end
 
 M.relativePath = function()
     -- Source: https://www.reddit.com/r/neovim/comments/q2s3t1/how_to_get_current_filename_relative_to_project/
-    copy(vim.fn.fnamemodify(vim.fn.expand("%"), ":p:~:."))
+    local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:~:.")
+    copy(relative_path)
 end
 
 M.fileName = function()
-    copy(vim.fn.expand("%:t"))
+    local file_name = vim.fn.expand("%:t")
+    copy(file_name)
 end
 
 return M
