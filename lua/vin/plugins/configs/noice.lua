0b39561bcfe014bbcd509bea2b11d2c82b8e66ee
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-04 16:37:32 +0100

    noice: disable notify tmp

--- lua/vin/plugins/configs/noice.lua
+++ lua/vin/plugins/configs/noice.lua
@@ -110,7 +110,7 @@ is_running_nightly(function()
             -- event is always "notify" and kind can be any log level as a string
             -- The default routes will forward notifications to nvim-notify
             -- Benefit of using Noice for this is the routing and consistent history view
-            enabled = true,
+            enabled = false,
             view = "notify",
         },
         lsp = {
