a02ed84cd51290490571ae5f1aed6df833218574
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-04 16:35:48 +0100

    tsserver: add documentation links

--- lua/vin/plugins/configs/lsp/tsserver.lua
+++ lua/vin/plugins/configs/lsp/tsserver.lua
@@ -5,6 +5,8 @@ end
 
 local disable_client_formating = Vin.lib.lsp.disable_client_formating
 
+---@see https://github.com/typescript-language-server/typescript-language-server#initializationoptions
+---@see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
 lsp.configure("tsserver", {
     on_init = function(client)
         -- We want to format with null-ls, so we disable the native formatter
