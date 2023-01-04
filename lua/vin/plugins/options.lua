932a5628a1c5a874a2e1a39b958f12ba1227de71
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-04 08:59:18 +0100

    lazy: update opts

--- lua/vin/plugins/options.lua
+++ lua/vin/plugins/options.lua
@@ -24,7 +24,7 @@ return {
         -- install missing plugins on startup. This doesn't increase startup time.
         missing = true,
         -- try to load one of these colorschemes when starting an installation during startup
-        colorscheme = { "habamax" },
+        colorscheme = { "terra", "habamax" },
     },
     ui = {
         -- a number <1 is a percentage., >1 is a fixed size
