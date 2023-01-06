85ad5eff0c170e61f5578147f04bc749876bf0fd
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-06 10:21:59 +0100

    lualine: switch separator

--- lua/vin/plugins/configs/lualine.lua
+++ lua/vin/plugins/configs/lualine.lua
@@ -173,10 +173,10 @@ lualine.setup({
         -- section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
         -- component_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
-        -- component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
         disabled_filetypes = {
             statusline = {
                 "nvim",
