cedfe0f90e6e03d349f0064d3f12df5920982420
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-07 09:56:44 +0100

    init: remove unused code for require in after

--- lua/vin/init.lua
+++ lua/vin/init.lua
@@ -15,7 +15,3 @@ require("vin.autocmds")
 require("vin.colorscheme")
 require("vin.cmds")
 require("vin.keymaps")
-
--- Allow require to look in after/plugin folder
-local home_dir = os.getenv("HOME")
-package.path = home_dir .. "/.config/nvim/after/plugin/?.lua;" .. package.path
