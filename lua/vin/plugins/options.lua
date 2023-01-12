dbb53ece0e7ac981663acdc2059305512ea8f80d
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-12 22:09:44 +0100

    plugins: use habamax

--- lua/vin/plugins/options.lua
+++ lua/vin/plugins/options.lua
@@ -24,7 +24,7 @@ return {
         -- install missing plugins on startup. This doesn't increase startup time.
         missing = true,
         -- try to load one of these colorschemes when starting an installation during startup
-        colorscheme = { "terra", "habamax" },
+        colorscheme = { "habamax" },
     },
     ui = {
         -- a number <1 is a percentage., >1 is a fixed size
