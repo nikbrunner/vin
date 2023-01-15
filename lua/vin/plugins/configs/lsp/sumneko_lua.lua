d1daea909c971efc5a4d670b6b769a46c277647e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-15 17:59:22 +0100

    sumneko: resolve custom settings

--- lua/vin/plugins/configs/lsp/sumneko_lua.lua
+++ lua/vin/plugins/configs/lsp/sumneko_lua.lua
@@ -10,22 +10,6 @@ lsp_zero.configure("sumneko_lua", {
         -- We want to format with null-ls, so we disable the native formatter
         disable_client_formating(client)
     end,
-    settings = {
-        Lua = {
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { "vim" },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-            completion = {
-                callSnippet = "Replace",
-            },
-        },
-    },
 })
 
 -- Setyup nvim workspace
