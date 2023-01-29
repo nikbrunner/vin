f14d11b062cb8e033ead57834c3961312a64366e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-01-29 15:34:31 +0100

    noice: update settings
    squash

--- lua/vin/plugins/ui.lua
+++ lua/vin/plugins/ui.lua
@@ -109,68 +109,70 @@ return {
         },
         opts = {
             lsp = {
+                progress = {
+                    enabled = false,
+                    -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+                    -- See the section on formatting for more details on how to customize.
+                    --- @type NoiceFormat|string
+                    format = "lsp_progress",
+                    --- @type NoiceFormat|string
+                    format_done = "lsp_progress_done",
+                    throttle = 1000 / 30, -- frequency to update lsp progress message
+                    view = "mini",
+                },
                 override = {
+                    -- override the default lsp markdown formatter with Noice
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    -- override the lsp markdown formatter with Noice
                     ["vim.lsp.util.stylize_markdown"] = true,
+                    -- override cmp documentation with Noice (needs the other options to work)
+                    ["cmp.entry.get_documentation"] = true,
+                },
+                hover = {
+                    enabled = true,
+                    view = nil, -- when nil, use defaults from documentation
+                    ---@type NoiceViewOptions
+                    opts = {}, -- merged with defaults from documentation
+                },
+                signature = {
+                    enabled = true,
+                    auto_open = {
+                        enabled = true,
+                        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                        throttle = 50, -- Debounce lsp signature help request by 50ms
+                    },
+                    view = nil, -- when nil, use defaults from documentation
+                    ---@type NoiceViewOptions
+                    opts = {}, -- merged with defaults from documentation
+                },
+                message = {
+                    -- Messages shown by lsp servers
+                    enabled = true,
+                    view = "notify",
+                    opts = {},
+                },
+                -- defaults for hover and signature help
+                documentation = {
+                    view = "hover",
+                    ---@type NoiceViewOptions
+                    opts = {
+                        lang = "markdown",
+                        replace = true,
+                        render = "plain",
+                        format = { "{message}" },
+                        win_options = { concealcursor = "n", conceallevel = 3 },
+                    },
                 },
             },
             presets = {
-                bottom_search = true,
-                command_palette = true,
-                long_message_to_split = true,
-            },
-        },
-        keys = {
-            {
-                "<S-Enter>",
-                function()
-                    require("noice").redirect(vim.fn.getcmdline())
-                end,
-                mode = "c",
-                desc = "Redirect Cmdline",
-            },
-            {
-                "<leader>snl",
-                function()
-                    require("noice").cmd("last")
-                end,
-                desc = "Noice Last Message",
-            },
-            {
-                "<leader>snh",
-                function()
-                    require("noice").cmd("history")
-                end,
-                desc = "Noice History",
-            },
-            {
-                "<leader>sna",
-                function()
-                    require("noice").cmd("all")
-                end,
-                desc = "Noice All",
-            },
-            {
-                "<c-f>",
-                function()
-                    if not require("noice.lsp").scroll(4) then
-                        return "<c-f>"
-                    end
-                end,
-                silent = true,
-                expr = true,
-                desc = "Scroll forward",
-            },
-            {
-                "<c-b>",
-                function()
-                    if not require("noice.lsp").scroll(-4) then
-                        return "<c-b>"
-                    end
-                end,
-                silent = true,
-                expr = true,
-                desc = "Scroll backward",
+                -- you can enable a preset by setting it to true, or a table that will override the preset config
+                -- you can also add custom presets that you can enable/disable with enabled=true
+                bottom_search = false, -- use a classic bottom cmdline for search
+                command_palette = false, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = true, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true, -- add a border to hover docs and signature help
             },
         },
     },
