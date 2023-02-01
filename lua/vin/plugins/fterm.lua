95bfb831f4ab04f4de9e594ec1db4d30655bf740
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-01 15:37:37 +0100

    fterm: format

--- lua/vin/plugins/fterm.lua
+++ lua/vin/plugins/fterm.lua
@@ -1,15 +1,14 @@
-
-    return {
-        "numToStr/FTerm.nvim",
-        opts = {
-            border = "single",
-            dimensions = {
-                height = 0.9,
-                width = 0.9,
-            },
+return {
+    "numToStr/FTerm.nvim",
+    opts = {
+        border = "single",
+        dimensions = {
+            height = 0.9,
+            width = 0.9,
         },
-        config = function(_, opts)
-            local fterm = require("FTerm")
-            fterm.setup(opts)
-        end,
-    }
+    },
+    config = function(_, opts)
+        local fterm = require("FTerm")
+        fterm.setup(opts)
+    end,
+}
