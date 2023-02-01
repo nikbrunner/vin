4ec017941a4d7a550ac95fbe9e69ff423f3c1db2
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-01 15:37:22 +0100

    lualine: reorder comment

--- lua/vin/plugins/lualine.lua
+++ lua/vin/plugins/lualine.lua
@@ -148,7 +148,8 @@
                 "filename",
                 file_status = true, -- Displays file status (readonly status, modified status)
                 newfile_status = false, -- Display new file status (new file means no write after created)
-                path = 1, -- 0: Just the filename
+                path = 1, 
+                -- 0: Just the filename
                 -- 1: Relative path
                 -- 2: Absolute path
                 -- 3: Absolute path, with tilde as the home directory
