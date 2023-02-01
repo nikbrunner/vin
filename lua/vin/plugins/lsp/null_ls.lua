6f88893cace7077930dd93f3d697e036bc017b81
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-01 15:37:07 +0100

    fix: try to fix formating

--- lua/vin/plugins/lsp/null_ls.lua
+++ lua/vin/plugins/lsp/null_ls.lua
@@ -1,8 +1,35 @@
 local M = {}
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 function M.setup(lsp_zero, config_sources)
     local null_ls = require("null-ls")
-    local null_opts = lsp_zero.build_options("null-ls", {})
+    local null_opts = lsp_zero.build_options("null-ls", {
+        on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                local format = function()
+                    vim.lsp.buf.format({
+                        id = client.id,
+                        bufnr = bufnr,
+                        async = true,
+                    })
+                end
+
+                vim.api.nvim_buf_create_user_command(bufnr, "NullFormat", format, {
+                    bang = true,
+                    range = true,
+                    desc = "Format using null-ls",
+                })
+
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    desc = "Auto format before save",
+                    pattern = "<buffer>",
+                    group = augroup,
+                    callback = format,
+                })
+            end
+        end,
+    })
 
     local buildSources = function(config)
         local sources = {}
@@ -19,31 +46,7 @@ function M.setup(lsp_zero, config_sources)
 
     null_ls.setup({
         sources = buildSources(config_sources),
-        on_attach = function(client, bufnr)
-            null_opts.on_attach(client, bufnr)
-
-            local format_cmd = function(input)
-                vim.lsp.buf.format({
-                    id = client.id,
-                    timeout_ms = 5000,
-                    async = input.bang,
-                })
-            end
-
-            vim.api.nvim_buf_create_user_command(bufnr, "NullFormat", format_cmd, {
-                bang = true,
-                range = true,
-                desc = "Format using null-ls",
-            })
-
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_create_autocmd("BufWritePost", {
-                    group = vim.api.nvim_create_augroup("NullFormatGroup", {}),
-                    buffer = bufnr,
-                    callback = format_cmd,
-                })
-            end
-        end,
+        on_attach = null_opts.on_attach,
     })
 end
 
