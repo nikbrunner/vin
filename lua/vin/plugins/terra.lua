9c77d833b2251f9bed0c241861dad5644fe4b99e
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-08 11:23:23 +0100

    terra: update

--- lua/vin/plugins/terra.lua
+++ lua/vin/plugins/terra.lua
@@ -1,14 +1,23 @@
-return {
-    -- "terra-theme/nvim",
-    dir = "~/Documents/dev/repos/personal/terra-theme/nvim",
+---@type LazySpec
+local spec = {
+    "terra-theme/nvim",
+
+    -- NOTE: Unfort. dev only looks for the repo name like `nvim` and not `terra-theme/nvim`, which requires adjustment in the LazyConfig `dev.path`
+    -- I wish the `dev.path` would be configurable on the LazySpec, but it is not.
+    -- Alternatively, I could just use a different `dir` for a specific local path, instead of using the `dev` property
+    dev = true,
     lazy = false,
     priority = 1000,
-    -- dev = true, -- TODO: Implement Dev Mode
+
+    ---@type TerraConfig
     opts = {
-        season = "winter",
+        season = "spring",
     },
+
     config = function(_, opts)
         local terra = require("terra")
         terra.setup(opts)
     end,
 }
+
+return spec
