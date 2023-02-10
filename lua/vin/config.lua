3bffe7302521513f3421fdccb33728dcd19949b5
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-10 14:50:31 +0100

    config: colorscheme terra

--- lua/vin/config.lua
+++ lua/vin/config.lua
@@ -9,7 +9,7 @@ local NOTES_PATH = join(DOCUMENTS_PATH, "notes/")
 ---@class VinConfig
 Vin.config = {
     username = "Nik",
-    colorscheme = "tokyonight",
+    colorscheme = "terra",
     nightly_version = "0.9",
     copilot = {
         suggestions = {
