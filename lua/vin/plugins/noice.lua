3950fe3c1f1f747031b7fd51b88f45f6fb37f698
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-11 13:37:45 +0100

    noice: throttle lsp status

--- lua/vin/plugins/noice.lua
+++ lua/vin/plugins/noice.lua
@@ -15,7 +15,7 @@ return {
                 format = "lsp_progress",
                 --- @type NoiceFormat|string
                 format_done = "lsp_progress_done",
-                throttle = 1000 / 30, -- frequency to update lsp progress message
+                throttle = 1000 / 10, -- frequency to update lsp progress message
                 view = "mini",
             },
             override = {
