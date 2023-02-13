9278e6854737e66aa5595f757434a25444003ea6
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-13 09:17:37 +0100

    plug(windows): disable animation
    squash windows

--- lua/vin/plugins/windows.lua
+++ lua/vin/plugins/windows.lua
@@ -3,7 +3,7 @@ return {
     event = "VeryLazy",
     dependencies = {
         "anuvyklack/middleclass",
-        "anuvyklack/animation.nvim",
+        -- "anuvyklack/animation.nvim",
     },
     opts = {
         autowidth = {
