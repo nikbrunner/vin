9a4691f97f2f35ef9ca1db8e7270742adf9e190f
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-03-07 19:49:08 +0100

    plug(terra): update spec

--- lua/vin/plugins/terra.lua
+++ lua/vin/plugins/terra.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 local spec = {
-    "terra-theme/nvim",
+    "terra-theme/terra.nvim",
 
     -- NOTE: Unfort. dev only looks for the repo name like `nvim` and not `terra-theme/nvim`, which requires adjustment in the LazyConfig `dev.path`
     -- I wish the `dev.path` would be configurable on the LazySpec, but it is not.
@@ -11,7 +11,7 @@ local spec = {
 
     ---@type TerraConfig
     opts = {
-        season = "winter",
+        season = "summer",
     },
 
     config = function(_, opts)
