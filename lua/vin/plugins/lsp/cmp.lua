c44d5ae786af9ebc109e564eff8941a9156826ba
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-03-30 21:17:06 +0200

    cmp: set additional options

--- lua/vin/plugins/lsp/cmp.lua
+++ lua/vin/plugins/lsp/cmp.lua
@@ -53,6 +53,11 @@ function M.setup(lsp_zero)
         },
     })
 
+    vim.lsp.handlers["textDocument/hover"] =
+        vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+    vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
     cmp.setup(cmp_config)
 end
 
