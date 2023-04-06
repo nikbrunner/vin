416498306ef7e9698abb8341a2d2ea30781458c1
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-06 19:07:30 +0200

    neotree: harpoon index & close on open file

--- lua/vin/plugins/neotree.lua
+++ lua/vin/plugins/neotree.lua
@@ -33,9 +33,42 @@ return {
         filesystem = {
             bind_to_cwd = false,
             follow_current_file = true,
+            components = {
+                harpoon_index = function(config, node, state)
+                    local Marked = require("harpoon.mark")
+                    local path = node:get_id()
+                    local succuss, index = pcall(Marked.get_index_of, path)
+                    if succuss and index and index > 0 then
+                        return {
+                            text = string.format(" ⥤ %d", index), -- <-- Add your favorite harpoon like arrow here
+                            highlight = config.highlight or "NeoTreeDirectoryIcon",
+                        }
+                    else
+                        return {}
+                    end
+                end,
+            },
+            renderers = {
+                file = {
+                    { "icon" },
+                    { "name", use_git_status_colors = true },
+                    { "harpoon_index" }, --> This is what actually adds the component in where you want it
+                    { "diagnostics" },
+                    { "git_status", highlight = "NeoTreeDimText" },
+                },
+            },
         },
         nesting_rules = {
             ["ts"] = { "spec.ts" },
+        },
+        event_handlers = {
+            {
+                event = "file_opened",
+                handler = function(file_path)
+                    --auto close
+                    require("neo-tree").close_all()
+                end,
+            },
         },
         window = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
             -- possible options. These can also be functions that return these options.
