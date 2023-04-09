67fbce0372634ec6403f7a52afbf96c80772c168
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-09 18:11:57 +0200

    glance: skip preview if only one defintion

--- lua/vin/plugins/glance.lua
+++ lua/vin/plugins/glance.lua
@@ -61,7 +61,15 @@ local spec = {
                     ["<leader>l"] = actions.enter_win("list"), -- Focus list window
                 },
             },
-            hooks = {},
+            hooks = {
+                before_open = function(results, open, jump, method)
+                    if #results == 1 then
+                        jump(results[1]) -- argument is optional
+                    else
+                        open(results) -- argument is optional
+                    end
+                end,
+            },
             folds = {
                 fold_closed = "",
                 fold_open = "",
