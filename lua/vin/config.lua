7aa2da836a3f739cb2609c2f81f8b79325764b2b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-14 22:16:20 +0200

    types: add
    squash

--- lua/vin/config.lua
+++ lua/vin/config.lua
@@ -11,7 +11,7 @@ Vin.config = {
     username = "Nik",
     colorscheme = "terra",
     nightly_version = "0.10",
-    dot_mode = true,
+    dot_mode = false,
     diagnostics = {
         virtual_text_enabled = true,
     },
---
+++ lua/vin/types.lua
@@ -0,0 +1,4 @@
+---@alias VinConfig.colorscheme "terra" | "tokyonight-moon" | "carbonfox" | "terafox"
+
+---@class VinConfig
+---@field colorscheme VinConfig.colorscheme Supported colorschemes
