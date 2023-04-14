5bbd04ce67e401029dcabe803a68baeb5a83754b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-14 22:23:53 +0200

    circles: enable only in dot_mode

--- lua/vin/plugins/circles_nvim.lua
+++ lua/vin/plugins/circles_nvim.lua
@@ -2,6 +2,9 @@
 local spec = {
     "projekt0n/circles.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    enabled = function()
+        return Vin.config.dot_mode
+    end,
     config = function()
         require("circles").setup()
     end,
