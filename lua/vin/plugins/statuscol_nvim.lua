e865e89141ce217c5effcc623a4fe8e70e1d9a10
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-19 08:37:22 +0200

    statuscol: simplify statuscol code

--- lua/vin/plugins/statuscol_nvim.lua
+++ lua/vin/plugins/statuscol_nvim.lua
@@ -3,16 +3,30 @@ local spec = {
     "luukvbaal/statuscol.nvim",
     event = "BufRead",
     enabled = true,
-    opts = function(self, opts)
+    opts = function()
         local builtin = require("statuscol.builtin")
         return {
             setopt = true, -- Whether to set the 'statuscolumn' option, may be set to false for those who
             relculright = true, -- whether to right-align the cursor line number with 'relativenumber' set
             segments = {
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-                { text = { " " } },
-                { text = { "%s" }, click = "v:lua.ScSa" },
-                { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                {
+                    text = { builtin.foldfunc, " " },
+                    click = "v:lua.ScFa",
+                    hl = "FoldColumn",
+                },
+                {
+                    text = { "%s" },
+                    hl = "SignColumn",
+                    click = "v:lua.ScSa",
+                },
+                {
+                    text = {
+                        builtin.lnumfunc,
+                        " ",
+                    },
+                    hl = "SignColumn",
+                    click = "v:lua.ScLa",
+                },
             },
         }
     end,
