8901967f7a79dbfa37f5232b08fae28e3427cfd8
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-27 09:46:03 +0200

    noice: update

--- lua/vin/plugins/noice.lua
+++ lua/vin/plugins/noice.lua
@@ -5,7 +5,53 @@ return {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
+    enabled = true,
     opts = {
+        cmdline = {
+            enabled = true, -- enables the Noice cmdline UI
+            view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+            opts = {}, -- global options for the cmdline. See section on views
+            ---@type table<string, CmdlineFormat>
+            format = {
+                -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+                -- view: (default is cmdline view)
+                -- opts: any options passed to the view
+                -- icon_hl_group: optional hl_group for the icon
+                -- title: set to anything or empty string to hide
+                cmdline = { pattern = "^:", icon = "", lang = "vim" },
+                search_down = {
+                    kind = "search",
+                    pattern = "^/",
+                    icon = " ",
+                    lang = "regex",
+                },
+                search_up = {
+                    kind = "search",
+                    pattern = "^%?",
+                    icon = " ",
+                    lang = "regex",
+                },
+                filter = { pattern = "^:%s!", icon = "$", lang = "bash" },
+                lua = {
+                    pattern = { "^:%slua%s+", "^:%slua%s=%s", "^:%s=%s" },
+                    icon = "",
+                    lang = "lua",
+                },
+                help = { pattern = "^:%she?l?p?%s+", icon = "" },
+                input = {}, -- Used by input()
+                -- lua = false, -- to disable a format, set to `false`
+            },
+        },
+        messages = {
+            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+            -- This is a current Neovim limitation.
+            enabled = true, -- enables the Noice messages UI
+            view = "notify", -- default view for messages
+            view_error = "notify", -- view for errors
+            view_warn = "notify", -- view for warnings
+            view_history = "messages", -- view for :messages
+            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+        },
         views = {
             cmdline_popup = {
                 border = {
@@ -106,7 +152,7 @@ return {
             -- you can enable a preset by setting it to true, or a table that will override the preset config
             -- you can also add custom presets that you can enable/disable with enabled=true
             bottom_search = false, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
+            command_palette = false, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = true, -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = true, -- add a border to hover docs and signature help
