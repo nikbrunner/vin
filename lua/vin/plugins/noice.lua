369b40cf97af033e610959cc85326aad4874fefc
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-05-02 17:44:37 +0200

    noice: disable notify

--- lua/vin/plugins/noice.lua
+++ lua/vin/plugins/noice.lua
@@ -7,51 +7,6 @@ return {
     },
     enabled = true,
     opts = {
-        cmdline = {
-            enabled = true, -- enables the Noice cmdline UI
-            view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-            opts = {}, -- global options for the cmdline. See section on views
-            ---@type table<string, CmdlineFormat>
-            format = {
-                -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-                -- view: (default is cmdline view)
-                -- opts: any options passed to the view
-                -- icon_hl_group: optional hl_group for the icon
-                -- title: set to anything or empty string to hide
-                cmdline = { pattern = "^:", icon = "", lang = "vim" },
-                search_down = {
-                    kind = "search",
-                    pattern = "^/",
-                    icon = " ",
-                    lang = "regex",
-                },
-                search_up = {
-                    kind = "search",
-                    pattern = "^%?",
-                    icon = " ",
-                    lang = "regex",
-                },
-                filter = { pattern = "^:%s!", icon = "$", lang = "bash" },
-                lua = {
-                    pattern = { "^:%slua%s+", "^:%slua%s=%s", "^:%s=%s" },
-                    icon = "",
-                    lang = "lua",
-                },
-                help = { pattern = "^:%she?l?p?%s+", icon = "" },
-                input = {}, -- Used by input()
-                -- lua = false, -- to disable a format, set to `false`
-            },
-        },
-        messages = {
-            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-            -- This is a current Neovim limitation.
-            enabled = true, -- enables the Noice messages UI
-            view = "notify", -- default view for messages
-            view_error = "notify", -- view for errors
-            view_warn = "notify", -- view for warnings
-            view_history = "messages", -- view for :messages
-            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-        },
         views = {
             cmdline_popup = {
                 border = {
@@ -63,6 +18,16 @@ return {
                     winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
                 },
             },
+        },
+        messages = {
+            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+            -- This is a current Neovim limitation.
+            enabled = false, -- enables the Noice messages UI
+            view = "notify", -- default view for messages
+            view_error = "notify", -- view for errors
+            view_warn = "notify", -- view for warnings
+            view_history = "messages", -- view for :messages
+            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
         },
         notify = {
             -- Noice can be used as `vim.notify` so you can route any notification like other messages
@@ -114,8 +79,8 @@ return {
             messages = {
                 -- NOTE: If you enable messages, then the cmdline is enabled automatically.
                 -- This is a current Neovim limitation.
-                enabled = true, -- enables the Noice messages UI
-                view = "messages", -- default view for messages
+                enabled = false, -- enables the Noice messages UI
+                view = "mini", -- default view for messages
                 view_error = "notify", -- view for errors
                 view_warn = "notify", -- view for warnings
                 view_history = "messages", -- view for :messages
