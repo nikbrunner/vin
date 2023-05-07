faf70f6988cf50ef4375ba141dfa362203cceb8b
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-05-07 13:19:55 +0200

    noice: udpate

--- lua/vin/plugins/noice.lua
+++ lua/vin/plugins/noice.lua
@@ -23,7 +23,7 @@ return {
             -- NOTE: If you enable messages, then the cmdline is enabled automatically.
             -- This is a current Neovim limitation.
             enabled = false, -- enables the Noice messages UI
-            view = "notify", -- default view for messages
+            view = "messages", -- default view for messages
             view_error = "notify", -- view for errors
             view_warn = "notify", -- view for warnings
             view_history = "messages", -- view for :messages
