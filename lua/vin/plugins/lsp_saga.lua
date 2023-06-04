716e033dfe27dca4f386394c3f3c6e0c023529cd
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-06-04 08:56:06 +0200

    plugs(saga): update opts

--- lua/vin/plugins/lsp_saga.lua
+++ lua/vin/plugins/lsp_saga.lua
@@ -7,6 +7,28 @@ local spec = {
     },
     event = "LspAttach",
     opts = {
+        finder = {
+            max_height = 0.5,
+            min_width = 30,
+            force_max_height = false,
+            keys = {
+                jump_to = "p",
+                expand_or_jump = "o",
+                vsplit = "v",
+                split = "s",
+                tabe = "t",
+                tabnew = "T",
+                quit = { "q", "<ESC>" },
+                close_in_preview = "<ESC>",
+            },
+        },
+        definition = {
+            edit = "<C-c>o",
+            vsplit = "<C-c>v",
+            split = "<C-c>s",
+            tabe = "<C-c>t",
+            quit = "q",
+        },
         lightbulb = {
             enable = false,
             enable_in_insert = false,
