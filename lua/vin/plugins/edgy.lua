92d1fae86391cf21bf258be4e5e060aea8b08c25
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-06-20 21:43:16 +0200

    plugs: add edgy

--- lua/vin/plugins/edgy.lua
+++ lua/vin/plugins/edgy.lua
@@ -1,123 +1,104 @@
----@type LazySpec
-local spec = {
-    "folke/edgy.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    opts = {
-        options = {
-            left = { size = Vin.config.sidebar_width },
-            bottom = { size = 10 },
-            right = { size = Vin.config.sidebar_width },
-            top = { size = 10 },
-        },
-
-        animate = {
-            enabled = true,
-            fps = 100, -- frames per second
-            cps = 120, -- cells per second
-            on_begin = function()
-                vim.g.minianimate_disable = true
-            end,
-            on_end = function()
-                vim.g.minianimate_disable = false
-            end,
-            -- Spinner for pinned views that are loading.
-            -- if you have noice.nvim installed, you can use any spinner from it, like:
-            -- spinner = require("noice.util.spinners").spinners.circleFull,
-            spinner = {
-                frames = {
-                    "⠋",
-                    "⠙",
-                    "⠹",
-                    "⠸",
-                    "⠼",
-                    "⠴",
-                    "⠦",
-                    "⠧",
-                    "⠇",
-                    "⠏",
+return {
+    -- edgy
+    {
+        "folke/edgy.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.opt.laststatus = 3
+            vim.opt.splitkeep = "screen"
+        end,
+        opts = {
+            bottom = {
+                {
+                    ft = "toggleterm",
+                    size = { height = 0.4 },
+                    filter = function(buf, win)
+                        return vim.api.nvim_win_get_config(win).relative == ""
+                    end,
                 },
-                interval = 80,
+                {
+                    ft = "noice",
+                    size = { height = 0.4 },
+                    filter = function(buf, win)
+                        return vim.api.nvim_win_get_config(win).relative == ""
+                    end,
+                },
+                {
+                    ft = "lazyterm",
+                    title = "LazyTerm",
+                    size = { height = 0.4 },
+                    filter = function(buf)
+                        return not vim.b[buf].lazyterm_cmd
+                    end,
+                },
+                "Trouble",
+                { ft = "qf", title = "QuickFix" },
+                {
+                    ft = "help",
+                    size = { height = 20 },
+                    -- don't open help files in edgy that we're editing
+                    filter = function(buf)
+                        return vim.bo[buf].buftype == "help"
+                    end,
+                },
+                { ft = "spectre_panel", size = { height = 0.4 } },
+                {
+                    title = "Neotest Output",
+                    ft = "neotest-output-panel",
+                    size = { height = 15 },
+                },
             },
-        },
-
-        bottom = {
-            -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
-            {
-                ft = "toggleterm",
-                size = { height = 0.4 },
-                -- exclude floating windows
-                filter = function(_, win)
-                    return vim.api.nvim_win_get_config(win).relative == ""
-                end,
+            left = {
+                {
+                    title = "Neo-Tree",
+                    ft = "neo-tree",
+                    filter = function(buf)
+                        return vim.b[buf].neo_tree_source == "filesystem"
+                    end,
+                    pinned = true,
+                    open = function()
+                        vim.api.nvim_input("<esc><space>e")
+                    end,
+                    size = { height = 0.5 },
+                },
+                { title = "Neotest Summary", ft = "neotest-summary" },
+                {
+                    title = "Neo-Tree Buffers",
+                    ft = "neo-tree",
+                    filter = function(buf)
+                        return vim.b[buf].neo_tree_source == "buffers"
+                    end,
+                    pinned = true,
+                    open = "Neotree position=top buffers",
+                },
+                {
+                    ft = "Outline",
+                    pinned = true,
+                    open = "SymbolsOutline",
+                },
             },
-            {
-                ft = "lazyterm",
-                title = "LazyTerm",
-                size = { height = 0.4 },
-                filter = function(buf)
-                    return not vim.b[buf].lazyterm_cmd
-                end,
-            },
-            "Trouble",
-            { ft = "qf", title = "QuickFix" },
-            {
-                ft = "help",
-                size = { height = 30 },
-                -- only show help buffers
-                filter = function(buf)
-                    return vim.bo[buf].buftype == "help"
-                end,
-            },
-            { ft = "spectre_panel", size = { height = 0.4 } },
-        },
-
-        left = {
-            {
-                title = "Files",
-                ft = "neo-tree",
-                pinned = true,
-                filter = function(buf, win)
-                    local isFileTree = vim.b[buf].neo_tree_source == "filesystem"
-
-                    -- if it is not floating and is a file tree
-                    return isFileTree
-                end,
-                size = { height = 0.5 },
-            },
-
-            {
-                title = "Buffers",
-                ft = "neo-tree",
-                pinned = true,
-                open = "Neotree position=top buffers",
-                filter = function(buf)
-                    return vim.b[buf].neo_tree_source == "buffers"
-                end,
-            },
-        },
-
-        ---@type (Edgy.View.Opts|string)[]
-        right = {
-            {
-                title = "Outline",
-                ft = "lspsagaoutline",
-                open = "Lspsaga outline",
-            },
-            {
-                title = "Git",
-                ft = "neo-tree",
-                pinned = true,
-                open = "Neotree position=right git_status",
-                size = { height = 0.5 },
-                filter = function(buf)
-                    local isFloat = vim.api.nvim_win_get_config(win).relative == ""
-                    print("Test: edgy.lua [[isFloat]]", isFloat)
-                    return vim.b[buf].neo_tree_source == "git_status"
-                end,
+            right = {
+                {
+                    title = "Neo-Tree Git",
+                    ft = "neo-tree",
+                    filter = function(buf)
+                        return vim.b[buf].neo_tree_source == "git_status"
+                    end,
+                    pinned = true,
+                    open = "Neotree position=right git_status",
+                },
             },
         },
     },
-}
 
-return spec
+    -- prevent neo-tree from opening files in edgy windows
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
+                or { "terminal", "Trouble", "qf", "Outline" }
+            table.insert(opts.open_files_do_not_replace_types, "edgy")
+        end,
+    },
+}
