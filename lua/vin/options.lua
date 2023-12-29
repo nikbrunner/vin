30546d4ab3f74aa0e215ab187d899be924cb8e94
nbr <nikolaus.brunner@protonmail.ch>
2023-12-29 20:09:09 +0100

    feat(options): add more options

--- lua/vin/options.lua
+++ lua/vin/options.lua
@@ -1,16 +1,55 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.cursorline = true
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
 
-vim.opt.autoread = true
+vim.opt.spelllang = "en,de"
+
+vim.opt.cursorline = true
+
+vim.opt.timeoutlen = 250 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.updatetime = 100 -- faster completion (4000ms default)
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.number = true -- set numbered lines
 vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
 vim.opt.relativenumber = true -- set relative numbered lines
+
+vim.opt.autoread = true
+vim.opt.backup = false -- creates a backup file
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.undofile = true -- enable persistent undo
+
+vim.opt.cmdheight = 1
+vim.opt.pumheight = 20 -- pop up menu height
+
+vim.opt.laststatus = 3 -- global statusline
+
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.wrap = false -- display lines as one long line
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.guifont = "JetBrains Mono:h16" -- the font used in graphical neovim applications,
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 24
+
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- smart case
+
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
