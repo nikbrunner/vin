b24b28f00e7506479b252196e3f0298dafd8d2e8
nbr <nikolaus.brunner@protonmail.ch>
2023-12-29 19:24:56 +0100

    feat(keymaps): add set util

--- lua/vin/keymaps.lua
+++ lua/vin/keymaps.lua
@@ -1,1 +1,9 @@
-vim.keymap.set("n", "<C-e>", "<cmd>Lex<CR>")
+local function set(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.noremap = opts.noremap == nil and true or opts.noremap
+    opts.silent = opts.silent == nil and true or opts.silent
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+local del = vim.keymap.del
+
+set("n", "<C-e>", "<cmd>Lex<CR>")
