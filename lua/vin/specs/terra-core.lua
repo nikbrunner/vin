895c381fc03337ff268242315ca2233933096e17
nbr <nikolaus.brunner@protonmail.ch>
2023-12-29 19:21:12 +0100

    feat(specs): add terra-theme

---
+++ lua/vin/specs/terra-core.lua
@@ -0,0 +1,46 @@
+---@diagnostic disable: missing-fields
+
+local M = {}
+
+---@type LazySpec
+M.spec = {
+    "terra-theme/terra-core.nvim",
+    dir = "~/Documents/dev/repos/terra-theme/terra-core.nvim",
+    lazy = false,
+    priority = 1000,
+    keys = {
+        {
+            "<leader>vtt",
+            function()
+                vim.cmd("Neotree close")
+                require("terra-core.utils.config").select_theme()
+            end,
+            desc = "Select theme",
+        },
+        {
+            "<leader>vtv",
+            function()
+                vim.cmd("Neotree close")
+                require("terra-core.utils.config").select_variant()
+            end,
+            desc = "Select variant",
+        },
+    },
+
+    ---@type TerraConfig
+    opts = {
+        debug = false,
+        theme = "fall",
+        variant = "night",
+        transparent = false,
+        diagnostics = {
+            background = true,
+        },
+    },
+
+    config = function(_, opts)
+        vim.cmd.colorscheme("terra_winter_night")
+    end,
+}
+
+return M.spec
