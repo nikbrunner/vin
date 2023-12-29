08e090e352d9557b518162dc6023da901a770aa1
nbr <nikolaus.brunner@protonmail.ch>
2023-12-29 19:28:58 +0100

    feat(options): add number options

--- lua/vin/options.lua
+++ lua/vin/options.lua
@@ -3,9 +3,14 @@ vim.g.maplocalleader = " "
 
 vim.opt.cursorline = true
 
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+
 vim.opt.autoread = true
+
+vim.opt.number = true -- set numbered lines
+vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.opt.relativenumber = true -- set relative numbered lines
