996861b10f95b55e66fcc68db26c6d5674d913cc
nbr <nikolaus.brunner@protonmail.ch>
2023-12-30 07:14:24 +0100

    feat(specs): add gitsigns

---
+++ lua/vin/specs/gitsigns.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+---@type LazySpec
+M.spec = {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        signs = {
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "" },
+            topdelete = { text = "" },
+            changedelete = { text = "▎" },
+            untracked = { text = "▎" },
+        },
+        on_attach = function(buffer)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, desc)
+                vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+            end
+
+            -- stylua: ignore start
+            map("n", "]g", gs.next_hunk, "Next Hunk")
+            map("n", "[g", gs.prev_hunk, "Prev Hunk")
+
+            map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+            map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+            map({ "n", "v" }, "<leader>gl", ":Gitsigns toggle_current_line_blame<CR>", "Current Line Blame")
+
+            map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+            map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+            map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+            map("n", "<leader>ghd", gs.diffthis, "Diff This")
+
+            map("n", "<leader>gbr", gs.reset_buffer, "Reset Buffer")
+            map("n", "<leader>gbs", gs.stage_buffer, "Stage Buffer")
+
+            map("n", "<leader>gd", function() gs.diffthis("~") end, "Diff This ~")
+        end,
+    },
+}
+
+return M.spec
