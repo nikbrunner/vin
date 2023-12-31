5a262696315c554910f3b7edf65d839d4d08f761
nbr <nikolaus.brunner@protonmail.ch>
2023-12-31 19:38:56 +0100

    feat(specs.barbecue): add

---
+++ lua/vin/specs/barbecue.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+---@type LazySpec
+M.spec = {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
+    opts = {
+        ---whether to show/use navic in the winbar
+        ---@type boolean
+        show_navic = false,
+
+        ---whether to attach navic to language servers automatically
+        ---@type boolean
+        attach_navic = false,
+
+        ---whether to create winbar updater autocmd
+        ---@type boolean
+        create_autocmd = true,
+
+        ---buftypes to enable winbar in
+        ---@type string[]
+        include_buftypes = {},
+
+        ---filetypes not to enable winbar in
+        ---@type string[]
+        exclude_filetypes = { "gitcommit", "toggleterm" },
+    },
+    config = function(_, opts)
+        require("barbecue").setup(opts)
+    end,
+}
+
+return M.spec
