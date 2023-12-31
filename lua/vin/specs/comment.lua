094800efef7ff890b3f12a4e4956a79c704a0821
nbr <nikolaus.brunner@protonmail.ch>
2023-12-31 20:54:20 +0100

    feat(specs.comment): replace with mini.comment

--- lua/vin/specs/comment.lua
+++ lua/vin/specs/comment.lua
@@ -2,8 +2,12 @@ local M = {}
 
 ---@type LazySpec
 M.spec = {
-    "numToStr/Comment.nvim",
-    config = true,
+    "echasnovski/mini.comment",
+    version = "*",
+    event = "BufEnter",
+    config = function(_, opts)
+        require("mini.comment").setup(opts)
+    end,
 }
 
 return M.spec
