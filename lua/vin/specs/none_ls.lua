2adeeed063267144b8a20bc2dec6c49adc776e9a
nbr <nikolaus.brunner@protonmail.ch>
2023-12-31 14:17:48 +0100

    feat(specs): enable autoformat

--- lua/vin/specs/none_ls.lua
+++ lua/vin/specs/none_ls.lua
@@ -5,6 +5,7 @@ M.spec = {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
         local code_actions = null_ls.builtins.code_actions
         local diagnostics = null_ls.builtins.diagnostics
@@ -12,6 +13,36 @@ M.spec = {
 
         -- To check state use `:NullLsInfo`
         null_ls.setup({
+
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    local format = function()
+                        vim.lsp.buf.format({
+                            id = client.id,
+                            bufnr = bufnr,
+                            async = true,
+                        })
+                    end
+
+                    vim.api.nvim_buf_create_user_command(
+                        bufnr,
+                        "NullFormat",
+                        format,
+                        {
+                            bang = true,
+                            range = true,
+                            desc = "Format using null-ls",
+                        }
+                    )
+
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        desc = "Auto format before save",
+                        pattern = "<buffer>",
+                        group = augroup,
+                        callback = format,
+                    })
+                end
+            end,
             sources = {
                 formatting.stylua,
                 diagnostics.luacheck,
