9cd3fa43292afd4057c85d0c901e22fae86f85f2
nbr <nikolaus.brunner@protonmail.ch>
2024-01-03 10:21:06 +0100

    feat(specs.mini): add mini.surround
    put all mini specs into one

---
+++ lua/vin/specs/mini.lua
@@ -0,0 +1,86 @@
+local M = {}
+
+---@type LazySpec[]
+M.specs = {
+    {
+        "echasnovski/mini.surround",
+        version = "*",
+        event = "VeryLazy",
+        opts = {
+            mappings = {
+                add = "gsa", -- Add surrounding in Normal and Visual modes
+                delete = "gsd", -- Delete surrounding
+                find = "gsf", -- Find surrounding (to the right)
+                find_left = "gsF", -- Find surrounding (to the left)
+                highlight = "gsh", -- Highlight surrounding
+                replace = "gsr", -- Replace surrounding
+                update_n_lines = "gsn", -- Update `n_lines`
+            },
+        },
+    },
+
+    {
+        "echasnovski/mini.clue",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            local miniclue = require("mini.clue")
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = "n", keys = "<Leader>" },
+                    { mode = "v", keys = "<Leader>" },
+                    { mode = "x", keys = "<Leader>" },
+
+                    -- Built-in completion
+                    { mode = "i", keys = "<C-x>" },
+
+                    -- `g` key
+                    { mode = "n", keys = "g" },
+                    { mode = "x", keys = "g" },
+
+                    -- Marks
+                    { mode = "n", keys = "'" },
+                    { mode = "n", keys = "`" },
+                    { mode = "x", keys = "'" },
+                    { mode = "x", keys = "`" },
+
+                    -- Registers
+                    { mode = "n", keys = '"' },
+                    { mode = "x", keys = '"' },
+                    { mode = "i", keys = "<C-r>" },
+                    { mode = "c", keys = "<C-r>" },
+
+                    -- Window commands
+                    { mode = "n", keys = "<C-w>" },
+
+                    -- `z` key
+                    { mode = "n", keys = "z" },
+                    { mode = "x", keys = "z" },
+                },
+
+                -- Clue window settings
+                window = {
+                    -- Delay before showing clue window
+                    delay = 250,
+
+                    -- Keys to scroll inside the clue window
+                    scroll_down = "<C-d>",
+                    scroll_up = "<C-u>",
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+            })
+        end,
+    },
+}
+
+return M.specs
