eafac18da006b4e4831c1bd308c37fac816e3050
nbr <nikolaus.brunner@protonmail.ch>
2024-01-03 14:03:04 +0100

    feat(specs.windows): add

---
+++ lua/vin/specs/windows.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+---@type LazySpec
+M.spec = {
+    "anuvyklack/windows.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "anuvyklack/middleclass",
+    },
+    keys = {
+        {
+            "<C-w>z",
+            "<cmd>WindowsMaximize<CR>",
+            desc = "Maximize Split",
+        },
+        {
+            "<leader>uW",
+            function()
+                vim.cmd("WindowsToggleAutowidth")
+                vim.notify("Windows Autowidth Toggled")
+            end,
+            desc = "Toggle Windows Autowidth",
+        },
+    },
+    opts = {
+        autowidth = {
+            enable = true,
+            winwidth = 15,
+        },
+        ignore = {
+            buftype = { "quickfix" },
+            filetype = { "NvimTree", "neo-tree", "undotree", "gundo" },
+        },
+    },
+    config = function(_, opts)
+        local windows = require("windows")
+
+        vim.o.winwidth = 10
+        vim.o.winminwidth = 10
+        vim.o.equalalways = false
+        windows.setup(opts)
+    end,
+}
+
+return M.spec
