48ff07860e374726a7805f6021a513fe9b1f5a27
nbr <nikolaus.brunner@protonmail.ch>
2024-01-04 15:09:19 +0100

    feat(specs.edgy): map help to bottom

--- lua/vin/specs/edgy.lua
+++ lua/vin/specs/edgy.lua
@@ -72,21 +72,46 @@ M.spec = {
         },
 
         bottom = {
+            "Trouble",
+
+            {
+                ft = "help",
+                size = { height = 20 },
+                -- only show help buffers
+                filter = function(buf)
+                    return vim.bo[buf].buftype == "help"
+                end,
+            },
+
+            {
+                ft = "bmessages_buffer",
+                title = "Messages",
+            },
+
+            {
+                ft = "qf",
+                title = "QuickFix",
+            },
+
+            {
+                ft = "spectre_panel",
+                size = { height = 0.4 },
+            },
+
+            {
+                title = "Neotest Output",
+                ft = "neotest-output-panel",
+                size = { height = 15 },
+            },
+
             {
                 ft = "toggleterm",
                 size = { height = 0.4 },
                 filter = function(buf, win)
                     return vim.api.nvim_win_get_config(win).relative == ""
                 end,
-                {
-                    ft = "help",
-                    size = { height = 0.25 },
-                    -- don't open help files in edgy that we're editing
-                    filter = function(buf)
-                        return vim.bo[buf].buftype == "help"
-                    end,
-                },
             },
+
             {
                 ft = "noice",
                 size = { height = 0.4 },
@@ -101,24 +126,6 @@ M.spec = {
                 filter = function(buf)
                     return not vim.b[buf].lazyterm_cmd
                 end,
-            },
-            "Trouble",
-            {
-                ft = "bmessages_buffer",
-                title = "Messages",
-            },
-            {
-                ft = "qf",
-                title = "QuickFix",
-            },
-            {
-                ft = "spectre_panel",
-                size = { height = 0.4 },
-            },
-            {
-                title = "Neotest Output",
-                ft = "neotest-output-panel",
-                size = { height = 15 },
             },
         },
     },
