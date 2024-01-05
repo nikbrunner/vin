55add53ad8fc71ec26ba5abc04f3810910ffc487
nbr <nikolaus.brunner@protonmail.ch>
2024-01-05 12:39:50 +0100

    feat(specs.neotree): update general opts

--- lua/vin/specs/neotree.lua
+++ lua/vin/specs/neotree.lua
@@ -76,151 +76,155 @@ M.spec = {
         }
     end,
     --- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/v3.x/lua/neo-tree/defaults.lua
-    opts = {
-        add_blank_line_at_top = true,
-        hide_root_node = true,
-        sources = {
-            "filesystem",
-            "buffers",
-            "git_status",
-            "document_symbols",
-        },
-        nesting_rules = {
-            ["package.json"] = {
-                pattern = "^package%.json$", -- <-- Lua pattern
-                files = { "package-lock.json", "yarn*" }, -- <-- glob pattern
-            },
-            ["go"] = {
-                pattern = "(.*)%.go$", -- <-- Lua pattern with capture
-                files = { "%1_test.go" }, -- <-- glob pattern with capture
-            },
-            ["ts"] = { "spec.ts" },
-        },
-        filesystem = {
-            hijack_netrw_behavior = "disabled",
-            follow_current_file = {
-                enabled = false, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
-            },
-            -- the current file is changed while the tree is open.
-            filtered_items = {
-                visible = false, -- when true, they will just be displayed differently than normal items
-                force_visible_in_empty_folder = false, -- when true, hidden files will be shown if the root folder is otherwise empty
-                show_hidden_count = true, -- when true, the number of hidden items in each folder will be shown as the last entry
-                hide_dotfiles = false,
-                hide_gitignored = true,
-                hide_hidden = false, -- only works on Windows for hidden files/directories
-                always_show = { -- remains visible even if other settings would normally hide it
-                    ".gitignored",
-                },
-                never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-                    ".DS_Store",
-                    "thumbs.db",
+    opts = function()
+        local icons = require("vin.icons")
+        return {
+            default_component_configs = {
+                git_status = {
+                    symbols = {
+                        added = icons.git.added,
+                        deleted = icons.git.removed,
+                        modified = icons.git.modified,
+                        renamed = icons.git.renamed,
+                        untracked = icons.git.untracked,
+                        ignored = icons.git.ignored,
+                        unstaged = icons.git.unstaged,
+                        staged = icons.git.staged,
+                        conflict = icons.git.conflict,
+                    },
                 },
             },
-        },
-        source_selector = {
-            winbar = false,
-            show_scrolled_off_parent_node = true,
-            statusline = false, -- toggle to show selector on statusline
-            content_layout = "center",
-            tabs_layout = "equal",
+            add_blank_line_at_top = true,
+            hide_root_node = true,
             sources = {
-                {
-                    source = "filesystem",
-                    display_name = "Files",
+                "filesystem",
+                "buffers",
+                "git_status",
+                "document_symbols",
+            },
+            nesting_rules = {
+                ["package.json"] = {
+                    pattern = "^package%.json$", -- <-- Lua pattern
+                    files = { "package-lock.json", "yarn*" }, -- <-- glob pattern
                 },
-                {
-                    source = "git_status",
-                    display_name = "Git",
+                ["go"] = {
+                    pattern = "(.*)%.go$", -- <-- Lua pattern with capture
+                    files = { "%1_test.go" }, -- <-- glob pattern with capture
                 },
-                {
-                    source = "buffers",
-                    display_name = "Buffers",
+                ["ts"] = { "spec.ts" },
+            },
+            filesystem = {
+                hijack_netrw_behavior = "disabled",
+                follow_current_file = {
+                    enabled = false, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
                 },
-                {
-                    source = "document_symbols",
-                    display_name = "Doc Symbols",
+                -- the current file is changed while the tree is open.
+                filtered_items = {
+                    visible = false, -- when true, they will just be displayed differently than normal items
+                    force_visible_in_empty_folder = false, -- when true, hidden files will be shown if the root folder is otherwise empty
+                    show_hidden_count = true, -- when true, the number of hidden items in each folder will be shown as the last entry
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_hidden = false, -- only works on Windows for hidden files/directories
+                    always_show = { -- remains visible even if other settings would normally hide it
+                        ".gitignored",
+                    },
+                    never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                        ".DS_Store",
+                        "thumbs.db",
+                    },
                 },
             },
-        },
+            source_selector = {
+                winbar = false,
+                show_scrolled_off_parent_node = true,
+                statusline = false, -- toggle to show selector on statusline
+                content_layout = "center",
+                tabs_layout = "equal",
+                sources = { -- table
+                    {
+                        source = "filesystem", -- string
+                        display_name = " 󰉓 Files ", -- string | nil
+                    },
+                    {
+                        source = "buffers", -- string
+                        display_name = " 󰈚 Buffers ", -- string | nil
+                    },
+                    {
+                        source = "git_status", -- string
+                        display_name = " 󰊢 Git ", -- string | nil
+                    },
+                },
+            },
 
-        event_handlers = {
-            {
-                event = "neo_tree_window_after_open",
-                handler = function()
-                    vim.cmd("wincmd =")
-                end,
+            event_handlers = {
+                {
+                    event = "neo_tree_window_after_open",
+                    handler = function()
+                        vim.cmd("wincmd =")
+                    end,
+                },
+
+                {
+                    event = "neo_tree_window_after_close",
+                    handler = function()
+                        vim.cmd("wincmd =")
+                    end,
+                },
             },
 
-            {
-                event = "neo_tree_window_after_close",
-                handler = function()
-                    vim.cmd("wincmd =")
-                end,
-            },
-        },
-
-        window = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
-            -- possible options. These can also be functions that return these options.
-            position = "left", -- left, right, top, bottom, float, current
-            width = 40, -- applies to left and right positions
-            popup = { -- settings that apply to float position only
-                size = { height = "25" },
-                position = "50%", -- 50% means center it
-            },
-            mappings = {
-                ["<space>"] = false,
-                ["l"] = "toggle_node",
-                ["<2-LeftMouse>"] = "open",
-                ["<cr>"] = "open",
-                ["L"] = "open",
-                ["<esc>"] = "revert_preview",
-                ["P"] = { "toggle_preview", config = { use_float = true } },
-                ["<C-s>"] = "open_split",
-                ["S"] = "split_with_window_picker",
-                ["<C-v>"] = "open_vsplit",
-                ["V"] = "vsplit_with_window_picker",
-                ["t"] = "open_tabnew",
-                ["w"] = "open_with_window_picker",
-                ["C"] = function(state)
-                    local node = state.tree:get_node()
-                    local filename_without_ext = node.name:gsub("%..*", "")
-                    lib.component.find_and_open_component_file(filename_without_ext)
-                end,
-                ["O"] = function(state)
-                    local node = state.tree:get_node()
-                    local path = node:get_id()
-                    vim.ui.open(path)
-                end,
-                ["M"] = "close_all_nodes",
-                ["R"] = "refresh",
-                ["a"] = { "add", config = { show_path = "relative" } }, -- "none", "relative", "absolute"
-                ["A"] = "add_directory", -- also accepts the config.show_path option.
-                ["d"] = "delete",
-                ["r"] = "rename",
-                ["y"] = "copy_to_clipboard",
-                ["x"] = "cut_to_clipboard",
-                ["p"] = "paste_from_clipboard",
-                ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path option
-                ["m"] = "move", -- takes text input for destination, also accepts the config.show_path option
-                ["e"] = "toggle_auto_expand_width",
-                ["q"] = "close_window",
-                ["?"] = "show_help",
-                ["<"] = "prev_source",
-                [">"] = "next_source",
-            },
-        },
-        document_symbols = {
-            window = {
+            window = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
+                -- possible options. These can also be functions that return these options.
+                position = "left", -- left, right, top, bottom, float, current
+                width = 40, -- applies to left and right positions
+                popup = { -- settings that apply to float position only
+                    size = { height = "25" },
+                    position = "50%", -- 50% means center it
+                },
                 mappings = {
-                    ["<cr>"] = "jump_to_symbol",
-                    ["o"] = "jump_to_symbol",
-                    ["/"] = "filter",
-                    ["f"] = "filter_on_submit",
+                    ["<space>"] = false,
+                    ["l"] = "toggle_node",
+                    ["<2-LeftMouse>"] = "open",
+                    ["<cr>"] = "open",
+                    ["L"] = "open",
+                    ["<esc>"] = "revert_preview",
+                    ["P"] = { "toggle_preview", config = { use_float = true } },
+                    ["<C-s>"] = "open_split",
+                    ["S"] = "split_with_window_picker",
+                    ["<C-v>"] = "open_vsplit",
+                    ["V"] = "vsplit_with_window_picker",
+                    ["t"] = "open_tabnew",
+                    ["w"] = "open_with_window_picker",
+                    ["C"] = function(state)
+                        local node = state.tree:get_node()
+                        local filename_without_ext = node.name:gsub("%..*", "")
+                        lib.component.find_and_open_component_file(filename_without_ext)
+                    end,
+                    ["O"] = function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        vim.ui.open(path)
+                    end,
+                    ["M"] = "close_all_nodes",
+                    ["R"] = "refresh",
+                    ["a"] = { "add", config = { show_path = "relative" } }, -- "none", "relative", "absolute"
+                    ["A"] = "add_directory", -- also accepts the config.show_path option.
+                    ["d"] = "delete",
+                    ["r"] = "rename",
+                    ["y"] = "copy_to_clipboard",
+                    ["x"] = "cut_to_clipboard",
+                    ["p"] = "paste_from_clipboard",
+                    ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path option
+                    ["m"] = "move", -- takes text input for destination, also accepts the config.show_path option
+                    ["e"] = "toggle_auto_expand_width",
+                    ["q"] = "close_window",
+                    ["?"] = "show_help",
+                    ["<"] = "prev_source",
+                    [">"] = "next_source",
                 },
             },
-        },
-    },
+        }
+    end,
     config = function(_, opts)
         require("neo-tree").setup(opts)
     end,
