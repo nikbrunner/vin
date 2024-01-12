9da41ab70d3b8a8ea4ea02580d7787fefeaac5d7
nbr <nikolaus.brunner@protonmail.ch>
2024-01-12 19:24:26 +0100

    feat(keymaps): disable some unused keymaps

--- lua/vin/keymaps.lua
+++ lua/vin/keymaps.lua
@@ -265,10 +265,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         set("n", "<leader>cf", "<CMD>NullFormat<CR>", vim.tbl_extend("force", opts, { desc = "Format" }))
 
-        set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-        set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
-        end, opts)
+        -- set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        -- set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        -- set("n", "<leader>wl", function()
+        --     print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
+        -- end, opts)
     end,
 })
