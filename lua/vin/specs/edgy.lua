997e603f51e4e4f0854b47d8fcdcf9b85daf5423
nbr <nikolaus.brunner@protonmail.ch>
2024-01-12 12:28:28 +0100

    feat(specs.edgy): dynamic sizes

--- lua/vin/specs/edgy.lua
+++ lua/vin/specs/edgy.lua
@@ -1,5 +1,26 @@
 local M = {}
 
+M.get_width = function(max_width)
+    max_width = max_width or 40
+    local relative_width = math.floor(vim.o.columns * 0.2)
+    if relative_width > max_width then
+        return max_width
+    else
+        return relative_width
+    end
+end
+
+M.get_height = function(max_height)
+    max_height = max_height or 10
+    local relative_height = math.floor(vim.o.lines * 0.2)
+
+    if relative_height > max_height then
+        return max_height
+    else
+        return relative_height
+    end
+end
+
 ---@type LazySpec
 M.spec = {
     "folke/edgy.nvim",
@@ -16,40 +37,28 @@ M.spec = {
             end,
             desc = "Edgy Toggle Windows",
         },
-        {
-            "<leader>us",
-            function()
-                require("edgy").select()
-            end,
-            desc = "Edgy Select Window",
-        },
     },
+    ---@type Edgy.Config
     opts = {
-        ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
-        animate = {
-            enabled = false,
-        },
+        animate = { enabled = false },
 
+        ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
         options = {
-            left = { size = 45 },
-            bottom = { size = 15 },
-            right = { size = 45 },
-            top = { size = 10 },
+            left = { size = M.get_width() },
+            bottom = { size = M.get_height() },
+            right = { size = M.get_width() },
+            top = { size = M.get_height() },
         },
 
         left = {
             {
                 title = "Files",
                 ft = "neo-tree",
+                pinned = true,
                 filter = function(buf, win)
                     local isFloat = vim.api.nvim_win_get_config(win).relative == ""
                     return isFloat and vim.b[buf].neo_tree_source == "filesystem"
                 end,
-                pinned = true,
-                open = function()
-                    vim.api.nvim_input("<esc><space>ee")
-                end,
-                size = { height = 0.5 },
             },
         },
 
@@ -57,12 +66,12 @@ M.spec = {
             {
                 title = "Git",
                 ft = "neo-tree",
+                pinned = true,
+                open = "Neotree position=right git_status",
                 filter = function(buf, win)
                     local isFloat = vim.api.nvim_win_get_config(win).relative == ""
                     return isFloat and vim.b[buf].neo_tree_source == "git_status"
                 end,
-                pinned = true,
-                open = "Neotree position=right git_status",
             },
             {
                 title = "Neotest Summary",
@@ -73,58 +82,35 @@ M.spec = {
 
         bottom = {
             "Trouble",
-
             {
                 ft = "help",
-                size = { height = 20 },
-                -- only show help buffers
                 filter = function(buf)
                     return vim.bo[buf].buftype == "help"
                 end,
+                size = { height = 0.4 },
             },
-
+            {
+                ft = "spectre_panel",
+                title = "Spectre",
+                size = { height = 0.4 },
+            },
             {
                 ft = "bmessages_buffer",
                 title = "Messages",
             },
-
             {
                 ft = "qf",
                 title = "QuickFix",
             },
-
-            {
-                ft = "spectre_panel",
-                size = { height = 0.4 },
-            },
-
             {
                 title = "Neotest Output",
                 ft = "neotest-output-panel",
-                size = { height = 15 },
             },
-
-            {
-                ft = "toggleterm",
-                size = { height = 0.4 },
-                filter = function(buf, win)
-                    return vim.api.nvim_win_get_config(win).relative == ""
-                end,
-            },
-
             {
                 ft = "noice",
                 size = { height = 0.4 },
-                filter = function(buf, win)
+                filter = function(_, win)
                     return vim.api.nvim_win_get_config(win).relative == ""
-                end,
-            },
-            {
-                ft = "lazyterm",
-                title = "LazyTerm",
-                size = { height = 0.4 },
-                filter = function(buf)
-                    return not vim.b[buf].lazyterm_cmd
                 end,
             },
         },
