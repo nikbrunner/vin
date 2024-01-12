caab98dc11a229ec167b4a7c429a5859a161cf24
nbr <nikolaus.brunner@protonmail.ch>
2024-01-12 11:46:57 +0100

    feat(specs.spectre): use current selection as prefill for search

--- lua/vin/specs/spectre.lua
+++ lua/vin/specs/spectre.lua
@@ -3,14 +3,32 @@ local M = {}
 ---@type LazySpec
 M.spec = {
     "nvim-pack/nvim-spectre",
-    build = false,
     cmd = "Spectre",
-    opts = { open_cmd = "noswapfile vnew" },
+    event = "BufRead",
+    opts = {
+        open_cmd = "noswapfile vnew",
+    },
     keys = {
         {
-            "<leader>sr",
+            "<leader>cR",
             function()
                 require("spectre").open()
+            end,
+            desc = "Replace in files (Spectre)",
+        },
+        {
+            "<leader>cR",
+            mode = { "v" },
+            function()
+                local function get_visual_selection()
+                    vim.cmd('noau normal! "vy"')
+
+                    return vim.fn.getreg("v")
+                end
+
+                require("spectre").open({
+                    search_text = get_visual_selection(),
+                })
             end,
             desc = "Replace in files (Spectre)",
         },
