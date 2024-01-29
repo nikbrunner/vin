bf5091f4537c056f181bc408c549f4183b158846
nbr <nikolaus.brunner@protonmail.ch>
2024-01-29 18:48:53 +0100

    chore(config): change colorscheme to terra_spring_night

--- lua/vin/config.lua
+++ lua/vin/config.lua
@@ -1,7 +1,7 @@
 ---@class VinConfig
 local config = {
-    ---@type TerraConfig.ColorSchemeName | "default"
-    colorscheme = "terra_winter_night",
+    ---@type TerraConfig.ColorSchemeName | "terra_spring_night"
+    colorscheme = "terra_spring_night",
     pathes = {
         notes = {
             personal = vim.fn.expand("~") .. "/Documents/notes/notes",
