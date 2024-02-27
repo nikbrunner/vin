e506d6a78be9b07a59c70f4baa93db6c7d68967a
nbr <nikolaus.brunner@protonmail.ch>
2024-02-27 10:44:33 +0100

    feat(specs.lint): add

---
+++ lua/vin/specs/lint.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+---@type LazyPluginSpec
+M.spec = {
+    "mfussenegger/nvim-lint",
+    event = "BufRead",
+    config = function()
+        local lint = require("lint")
+
+        -- I was trying to set this up via `opts` but I have found no way to do it.
+        lint.linters_by_ft = {
+            lua = { "luacheck" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
+            group = vim.api.nvim_create_augroup("vin_lint", { clear = true }),
+            callback = function()
+                require("lint").try_lint()
+            end,
+        })
+    end,
+}
+
+return M.spec
