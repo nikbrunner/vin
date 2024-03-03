1a09444af852c96b23e9e342187a43b21a71cce8
nbr <nikolaus.brunner@protonmail.ch>
2024-03-03 15:39:37 +0100

    feat(lazy-lock): update

--- lua/vin/specs/fzf.lua
+++ lua/vin/specs/fzf.lua
@@ -39,8 +39,8 @@ M.win_presets = {
     },
     medium = {
         flex = {
-            height = 0.95,
-            width = 0.75,
+            height = 0.65,
+            width = 0.65,
             preview = {
                 layout = "flex",
                 vertical = "up:50%",
@@ -133,8 +133,8 @@ M.spec = {
             global_resume_query = true, -- include typed query in `resume`?
 
             winopts = {
-                height = 0.75, -- window height
-                width = 0.75, -- window width
+                height = 0.65, -- window height
+                width = 0.65, -- window width
                 row = 0.35, -- window row position (0=top, 1=bottom)
                 col = 0.50, -- window col position (0=left, 1=right)
                 border = "single",
