96777f7c31852dfaaba4b6932e4d2e549c4c3a7f
nbr <nikolaus.brunner@protonmail.ch>
2024-03-03 16:44:24 +0100

    feat(ftplugin): add markdown specific options

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+local map = vim.keymap.set
+
+map("n", "<C-j>", [[/^##\+ .*<CR>]], { buffer = true, silent = true })
+map("n", "<C-k>", [[?^##\+ .*<CR>]], { buffer = true, silent = true })
+map({ "n", "o", "x" }, "j", "gj", {})
+map({ "n", "o", "x" }, "k", "gk", {})
+map({ "n", "o", "x" }, "0", "g0", {})
+map({ "n", "o", "x" }, "$", "g$", {})
+
+vim.opt.wrap = true
