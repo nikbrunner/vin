3ed3b168d7ca842be724e102b754b71ea431211d
nbr <nikolaus.brunner@protonmail.ch>
2024-03-29 13:38:13 +0100

    specs(arrow): <leader>m

--- lua/vin/specs/arrow.lua
+++ lua/vin/specs/arrow.lua
@@ -50,7 +50,7 @@ M.spec = {
         },
     },
     opts = {
-        leader_key = "<leader>mm",
+        leader_key = "<leader>m",
         show_icons = true,
         always_show_path = true,
         separate_by_branch = true,
