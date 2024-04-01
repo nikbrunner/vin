45da04d7c80957990e5b448032500ffb5c8cfb8d
nbr <nikolaus.brunner@protonmail.ch>
2024-04-01 14:16:24 +0200

    ftplugin(markdown): respect first headers

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
-map("n", "<C-j>", [[/^##\+ .*<CR>]], { buffer = true, silent = true })
-map("n", "<C-k>", [[?^##\+ .*<CR>]], { buffer = true, silent = true })
+map("n", "<C-j>", [[/^#\+ .*<CR>]], { buffer = true, silent = true })
+map("n", "<C-k>", [[?^#\+ .*<CR>]], { buffer = true, silent = true })
 map({ "n", "o", "x" }, "j", "gj", {})
 map({ "n", "o", "x" }, "k", "gk", {})
 map({ "n", "o", "x" }, "0", "g0", {})
