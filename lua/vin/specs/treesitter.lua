0e53568e48dc41626fc2424cc6765c5fc3ccaff4
nbr <nikolaus.brunner@protonmail.ch>
2024-04-18 17:23:45 +0200

    specs(treesitter_context): enable

--- lua/vin/specs/treesitter.lua
+++ lua/vin/specs/treesitter.lua
@@ -170,10 +170,10 @@ M.specs = {
         "nvim-treesitter/nvim-treesitter-context",
         dependencies = "nvim-treesitter/nvim-treesitter",
         event = "VeryLazy",
-        enabled = false,
+        enabled = true,
         opts = {
             enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-            max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
+            max_lines = 4, -- How many lines the window should span. Values <= 0 mean no limit.
             multiline_threshold = 20, -- Maximum number of lines to show for a single context
             trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
             zindex = 20, -- The Z-index of the context window
