516e44b35eb99d5617367a20ebb3dc3075f67631
nbr <nikolaus.brunner@protonmail.ch>
2024-05-03 17:29:41 +0200

    specs(arrow): update config

--- lua/vin/specs/arrow.lua
+++ lua/vin/specs/arrow.lua
@@ -3,44 +3,10 @@ local M = {}
 ---@type LazyPluginSpec
 M.spec = {
     "otavioschwanck/arrow.nvim",
-    dependencies = {
-        "WolfeCub/harpeek.nvim",
-        keys = {
-            {
-                "<leader>M",
-                function()
-                    require("harpeek").toggle()
-                end,
-                desc = "[M]arks Preview",
-            },
-        },
-        opts = {
-            -- You can replace the hightlight group used on the currently selected buffer
-            hl_group = "CursorLineNr",
-            -- You can override any window options. For example here we set a different position & border.
-            winopts = {
-                title = "Bookmarks",
-                row = 0.85,
-                col = vim.o.columns,
-                border = "solid",
-            },
-            format = "relative",
-        },
-        config = function(_, opts)
-            require("harpeek").setup(opts)
-        end,
-    },
-    keys = {
-        {
-            "<leader>m",
-            function()
-                require("arrow.ui").openMenu()
-            end,
-            desc = "Marks",
-        },
-    },
+    event = "BufEnter",
     opts = {
-        leader_key = nil,
+        leader_key = ";",
+        buffer_leader_key = "m",
         show_icons = false,
         always_show_path = true,
         separate_by_branch = true,
