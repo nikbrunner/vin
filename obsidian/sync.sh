80803619f3c5542e845fcd0decd45059b617d6f6
nbr <nikolaus.brunner@protonmail.ch>
2024-05-12 11:04:26 +0200

    feat: SSOT for `.obsidian.vimrc`

---
+++ obsidian/sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script copies the .vimrc file from my Neovim configuration to the two Obsidian vaults I have.
+
+cp ~/.config/nvim/obsidian/.vimrc ~/repos/nikbrunner/notes/.obsidian.vimrc
+cp ~/.config/nvim/obsidian/.vimrc ~/repos/nikbrunner/dcd-notes/.obsidian.vimrc
+
+echo "Synced Obsidian .vimrc to vaults."
