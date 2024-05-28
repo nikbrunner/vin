033087a685610b7bec9c4235924357542328735e
nbr <nikolaus.brunner@protonmail.ch>
2024-05-28 10:54:55 +0200

    specs(supermaven): try out supermaven

---
+++ lua/vin/specs/supermaven.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+---@type LazyPluginSpec
+M.spec = {
+    "supermaven-inc/supermaven-nvim",
+    event = "InsertEnter",
+    opts = {
+        keymaps = {
+            accept_suggestion = "<Tab>",
+            clear_suggestion = "<C-]>",
+            accept_word = "<C-j>",
+        },
+        ignore_filetypes = {},
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+    },
+    config = function(_, opts)
+        require("supermaven-nvim").setup(opts)
+    end,
+}
+
+return M.spec
