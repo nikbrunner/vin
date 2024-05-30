2ca64f8e07d7e259d30d126b2aaaaa3ff52162cf
nbr <nikolaus.brunner@protonmail.ch>
2024-05-30 12:31:43 +0200

    lib(ui): add maximize window command

--- lua/vin/lib/init.lua
+++ lua/vin/lib/init.lua
@@ -5,6 +5,7 @@ local M = {
     colors = require("vin.lib.colors"),
     files = require("vin.lib.files"),
     git = require("vin.lib.git"),
+    ui = require("vin.lib.ui"),
 }
 
 return M
--- lua/vin/lib/ui.lua
+++ lua/vin/lib/ui.lua
@@ -12,4 +12,21 @@ function M.handle_colors(config, colorscheme, background)
     require("vin.lib.files").sync_wezterm_colorscheme(config, colorscheme)
 end
 
+M._maximized_window = nil
+function M.maximize_window()
+    if M._maximized_window then
+        vim.o.winwidth = M._maximized_window.width
+        vim.o.winheight = M._maximized_window.height
+        M._maximized_window = nil
+        vim.cmd("wincmd =")
+    else
+        M._maximized_window = {
+            width = vim.o.winwidth,
+            height = vim.o.winheight,
+        }
+        vim.o.winwidth = 999
+        vim.o.winheight = 999
+    end
+end
+
 return M
