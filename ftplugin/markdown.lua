56d6cda6662eeb8db6f818d7832933015eaa0978
nbr <nikolaus.brunner@protonmail.ch>
2024-06-01 20:21:37 +0200

    ftplugin(markdown): remove bindings and no conceal

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,11 +1,9 @@
 local map = vim.keymap.set
 
-map("n", "<C-j>", [[/^#\+ .*<CR>]], { buffer = true, silent = true })
-map("n", "<C-k>", [[?^#\+ .*<CR>]], { buffer = true, silent = true })
 map({ "n", "o", "x" }, "j", "gj", {})
 map({ "n", "o", "x" }, "k", "gk", {})
 map({ "n", "o", "x" }, "0", "g0", {})
 map({ "n", "o", "x" }, "$", "g$", {})
 
 vim.opt.wrap = true
-vim.opt.conceallevel = 2
+vim.opt.conceallevel = 0
