97bb5ab2065d86ad41fa531b4ed0a43640969cc8
nbr <nikolaus.brunner@protonmail.ch>
2024-06-01 20:21:46 +0200

    config: update

--- lua/vin/config.lua
+++ lua/vin/config.lua
@@ -5,7 +5,7 @@ local base_pathes = {
 
 ---@class VinConfig
 local config = {
-    colorscheme = "black-atom-engineering",
+    colorscheme = "terra-spring-night",
     ---@type "dark" | "light"
     background = "dark",
     ---see for wezterm themes: https://wezfurlong.org/wezterm/colorschemes/index.html
