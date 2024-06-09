0fc2d0732717e8895ef178305f0a1e7ecbb274a9
nbr <nikolaus.brunner@protonmail.ch>
2024-06-09 20:25:52 +0200

    specs(mini.files): remove

--- lua/vin/specs/mini.lua
+++ lua/vin/specs/mini.lua
@@ -20,55 +20,6 @@ M.specs = {
     },
 
     {
-        "echasnovski/mini.files",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            mappings = {
-                close = "q",
-                go_in = "l",
-                go_in_plus = "<CR>",
-                go_out = "h",
-                go_out_plus = "H",
-                reset = "<BS>",
-                reveal_cwd = "@",
-                show_help = "g?",
-                synchronize = "=",
-                trim_left = "<",
-                trim_right = ">",
-            },
-            options = {
-                -- Whether to delete permanently or move into module-specific trash
-                permanent_delete = true,
-                -- Whether to use for editing directories
-                use_as_default_explorer = false,
-            },
-            -- Customization of explorer windows
-            windows = {
-                -- Maximum number of windows to show side by side
-                max_number = math.huge,
-                -- Whether to show preview of file/directory under cursor
-                preview = false,
-                -- Width of focused window
-                width_focus = 50,
-                -- Width of non-focused window
-                width_nofocus = 15,
-                -- Width of preview window
-                width_preview = 25,
-            },
-        },
-        keys = {
-            {
-                "<C-e>",
-                function()
-                    require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
-                end,
-                desc = "[E]xplorer",
-            },
-        },
-    },
-
-    {
         "echasnovski/mini.notify",
         version = "*",
         lazy = false,
