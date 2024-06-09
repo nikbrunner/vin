6cb672513a85a2662f7c14bf02f885d3337755a5
nbr <nikolaus.brunner@protonmail.ch>
2024-06-09 19:19:56 +0200

    specs(text-to-colorscheme): add

--- lua/vin/lib/files.lua
+++ lua/vin/lib/files.lua
@@ -21,8 +21,18 @@ end
 ---@param colorscheme string
 function M.sync_wezterm_colorscheme(config, colorscheme)
     local pathes = config.pathes
+    local bg_normal = require("vin.lib.colors").get_hex_color("Normal").bg
+    local bg_normal_float = require("vin.lib.colors").get_hex_color("NormalFloat").bg
 
     local colorscheme_config = config.colorscheme_config_map[colorscheme]
+
+    if bg_normal then
+        M.update_line_in_file(pathes.config.wezterm, "background", '"' .. bg_normal .. '"')
+    elseif bg_normal_float then
+        M.update_line_in_file(pathes.config.wezterm, "background", '"' .. bg_normal_float .. '"')
+    else
+        print("Could not find background color.")
+    end
 
     if colorscheme_config and colorscheme_config.wezterm then
         M.update_line_in_file(pathes.config.wezterm, "color_scheme", '"' .. colorscheme_config.wezterm .. '"')
