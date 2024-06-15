d6ce8e5201429d402476bedf1bf17eae53365743
nbr <nikolaus.brunner@protonmail.ch>
2024-06-15 20:15:43 +0200

    specs(focus): disable some buf & filetypes

--- lua/vin/specs/focus.lua
+++ lua/vin/specs/focus.lua
@@ -18,6 +18,38 @@ M.spec = {
             signcolumn = false, -- Display signcolumn in the focussed window only
         },
     },
+    config = function(_, opts)
+        require("focus").setup(opts)
+
+        local ignore_filetypes = { "neo-tree", "trouble" }
+        local ignore_buftypes = { "nofile", "prompt", "popup", "quickfix" }
+
+        local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+
+        vim.api.nvim_create_autocmd("WinEnter", {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+                    vim.w.focus_disable = true
+                else
+                    vim.w.focus_disable = false
+                end
+            end,
+            desc = "Disable focus autoresize for BufType",
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                    vim.b.focus_disable = true
+                else
+                    vim.b.focus_disable = false
+                end
+            end,
+            desc = "Disable focus autoresize for FileType",
+        })
+    end,
     keys = {
         {
             "M",
