ef231452db1d0402e9d61f36299ca23023125c33
nbr <nikolaus.brunner@protonmail.ch>
2024-06-16 11:49:05 +0200

    specs(yazi): update config

--- lua/vin/specs/yazy.lua
+++ lua/vin/specs/yazy.lua
@@ -23,10 +23,12 @@ M.spec = {
             desc = "[E]xplorer (CWD)",
         },
     },
+    -- TODO: Image Preview does not work: https://github.com/mikavilpas/yazi.nvim/issues/112
     ---@type YaziConfig
     opts = {
-        yazi_floating_window_winblend = 20,
+        yazi_floating_window_winblend = 10,
         floating_window_scaling_factor = 0.8,
+        yazi_floating_window_border = "solid",
     },
 }
 
