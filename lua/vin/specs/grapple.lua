ef13f81d3373db68c0bb676ac5ed99c276dd097a
nbr <nikolaus.brunner@protonmail.ch>
2024-07-02 09:00:05 +0200

    specs(grapple): general update

--- lua/vin/specs/grapple.lua
+++ lua/vin/specs/grapple.lua
@@ -1,4 +1,7 @@
-return {
+local M = {}
+
+---@type LazyPluginSpec
+M.spec = {
     "cbochs/grapple.nvim",
     opts = {
         scope = "git_branch", -- also try out "git_branch"
@@ -6,9 +9,11 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-        { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
-        { "<leader>M", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
-        -- { "<C-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-        -- { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+        { "<leader>ma", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+        { "<leader>mm", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
+        { "[m", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+        { "]m", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
     },
 }
+
+return M.spec
