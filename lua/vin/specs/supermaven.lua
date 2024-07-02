44c944d0f8b8b5b8eb08b08c07b618e936b04f2e
nbr <nikolaus.brunner@protonmail.ch>
2024-07-02 09:05:18 +0200

    specs(supermaven): add mapping to toggle autocompletion

--- lua/vin/specs/supermaven.lua
+++ lua/vin/specs/supermaven.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+M.toggle_inline_ai = function()
+    local suggestion = require("supermaven-nvim.completion_preview")
+    local message = "Inline AI autocompletion "
+
+    if suggestion.disable_inline_completion then
+        suggestion.disable_inline_completion = false
+        vim.notify(message .. "ENABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+    else
+        suggestion.disable_inline_completion = true
+        vim.notify(message .. "DISABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+    end
+end
+
 ---@type LazyPluginSpec
 M.spec = {
     "supermaven-inc/supermaven-nvim",
@@ -14,6 +27,9 @@ M.spec = {
         ignore_filetypes = {},
         disable_inline_completion = false, -- disables inline completion for use with cmp
         disable_keymaps = false, -- disables built in keymaps for more manual control
+    },
+    keys = {
+        { "<leader>cia", M.toggle_inline_ai, desc = "[C]ode [I]ntelligence [A]utocompletion" },
     },
     config = function(_, opts)
         require("supermaven-nvim").setup(opts)
