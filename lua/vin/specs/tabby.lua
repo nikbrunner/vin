af591a0d876665e19f3a2fae92a126b2445cc7e3
nbr <nikolaus.brunner@protonmail.ch>
2024-07-04 12:06:35 +0200

    specs(tabby): resurrect

--- lua/vin/specs/tabby.lua
+++ lua/vin/specs/tabby.lua
@@ -5,7 +5,6 @@ M.spec = {
     "nanozuki/tabby.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     event = "VimEnter",
-    enabled = false,
     config = function()
         vim.opt.showtabline = 2
         require("tabby.tabline").use_preset("active_wins_at_tail", {
