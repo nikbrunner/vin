e8314ce5cd8720d5f64ec3f845c818349a614a39
nbr <nikolaus.brunner@protonmail.ch>
2024-07-22 09:54:29 +0200

    config: update colorscheme

--- lua/vin/config.lua
+++ lua/vin/config.lua
@@ -2,7 +2,7 @@ local M = {}
 
 ---@class VinConfig
 M.config = {
-    colorscheme = "black-atom-engineering",
+    colorscheme = "black-atom-operations",
     ---@type "dark" | "light"
     background = "dark",
     ---see for wezterm themes: https://wezfurlong.org/wezterm/colorschemes/index.html
