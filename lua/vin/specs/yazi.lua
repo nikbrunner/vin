5e884531892f5c31c70af8ed917357ae85799f28
nbr <nikolaus.brunner@protonmail.ch>
2024-07-23 08:35:32 +0200

    specs(yazi): backdrop

--- lua/vin/specs/yazi.lua
+++ lua/vin/specs/yazi.lua
@@ -1,3 +1,5 @@
+local ui = require("vin.lib.ui")
+
 local M = {}
 
 ---@type LazySpec
@@ -31,6 +33,18 @@ M.spec = {
         yazi_floating_window_winblend = 10,
         floating_window_scaling_factor = 0.8,
         yazi_floating_window_border = "solid",
+        ---@diagnostic disable-next-line: missing-fields
+        hooks = {
+            ---@diagnostic disable-next-line: unused-local
+            yazi_opened = function(preselected_path, buffer, config)
+                ui.create_backdrop()
+                vim.cmd("Neotree close")
+            end,
+            ---@diagnostic disable-next-line: unused-local
+            yazi_closed_successfully = function(chosen_file, config, state)
+                ui.close_backdrop()
+            end,
+        },
     },
 }
 
