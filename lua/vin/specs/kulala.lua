04655394cd557311ebab911826c8f3e058bb7b15
nbr <nikolaus.brunner@protonmail.ch>
2024-08-16 14:59:19 +0200

    specs(kulala): update mappings

--- lua/vin/specs/kulala.lua
+++ lua/vin/specs/kulala.lua
@@ -10,10 +10,13 @@ return {
         local kulala = require("kulala")
 
         return {
-            { "[r", kulala.jump_prev, desc = "Previous request" },
+            { "[h", kulala.jump_prev, desc = "Previous request" },
+            { "]h", kulala.jump_next, desc = "Next request" },
             { "<leader>hh", kulala.run, desc = "Run request" },
-            { "<leader>hs", kulala.search, desc = "Run request" },
-            { "]r", kulala.jump_next, desc = "Next request" },
+            { "<leader>h.", kulala.replay, desc = "Re-Run request" },
+            { "<leader>he", kulala.set_selected_env, desc = "Select env" },
+            { "<leader>hs", kulala.search, desc = "Search" },
+            { "<leader>hc", kulala.copy, desc = "Copy request" },
         }
     end,
 }
