7af88ccf24b552b426f72a8004b5860680236dc8
nbr <nikolaus.brunner@protonmail.ch>
2024-08-18 19:42:59 +0200

    specs(markview): add toggle mapping

--- lua/vin/specs/markview.lua
+++ lua/vin/specs/markview.lua
@@ -4,6 +4,13 @@ local M = {}
 M.spec = {
     "OXY2DEV/markview.nvim",
     ft = "markdown",
+    keys = {
+        {
+            "<leader>um",
+            ":Markview toggle<CR>",
+            desc = "Toggle Markiew",
+        },
+    },
 }
 
 return M.spec
