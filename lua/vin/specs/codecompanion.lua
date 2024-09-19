64d431568ae650a3ec178c68407837a93b6fdaf2
nbr <nikolaus.brunner@protonmail.ch>
2024-09-19 21:22:59 +0200

    deps(codecompanion): enhance commit message generation and disable gp.nvim
    - refactor(codecompanion): improve pre-defined prompt for commit message generation
    - feat(codecompanion): add `telescope.nvim` dependency
    - style(codecompanion): update key mappings descriptions
    - fix(gp): disable `gp.nvim` plugin

--- lua/vin/specs/codecompanion.lua
+++ lua/vin/specs/codecompanion.lua
@@ -3,11 +3,11 @@ local M = {}
 ---@type LazyPluginSpec
 M.spec = {
     "olimorris/codecompanion.nvim",
-    enabled = false,
     event = "VeryLazy",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
         { "stevearc/dressing.nvim", opts = {} },
     },
     -- Source: https://github.com/olimorris/codecompanion.nvim?tab=readme-ov-file#hammer_and_wrench-defaults
@@ -20,36 +20,71 @@ M.spec = {
                 adapter = "openai",
             },
         },
-        keymaps = {
-            send = {
-                modes = {
-                    n = "<C-s>",
-                    i = "<C-s>",
-                },
-                index = 1,
-                callback = "keymaps.send",
-                description = "Send",
-            },
-        },
-        default_prompts = {
+        pre_defined_prompts = {
             ["Generate a Commit Message"] = {
                 strategy = "chat",
                 description = "Generate a commit message",
                 opts = {
-                    index = 5,
-                    default_prompt = true,
-                    mapping = "<LocalLeader>cm",
+                    mapping = "<leader>im",
+                    modes = { "n" },
                     slash_cmd = "commit",
                     auto_submit = true,
                 },
                 prompts = {
                     {
-                        role = "${user}",
-                        contains_code = true,
+                        role = "system",
                         content = function()
-                            return "You are an expert at following the Conventional Commit specification. Given the git diff listed below, without detailed explanation, please generate a commit message for me:"
-                                .. "\n\n```\n"
-                                .. vim.fn.system("git diff")
+                            local has_staged_changes = vim.fn.system("git diff --staged") ~= ""
+
+                            if not has_staged_changes then
+                                vim.notify(
+                                    "Error while generating commit message: No staged changes found"
+                                        .. "\n\n"
+                                        .. "Please stage your changes before running this command.",
+                                    vim.log.levels.ERROR
+                                )
+                                return
+                            end
+
+                            local base_template = "You are an expert at following the Conventional Commit specification."
+                                .. "Given the git diff listed below, please generate a detailed commit message for me and return it to me directly without explanation:"
+                                .. "Use the summary line to describe the overall change, followed by an empty line, and then a more detailed, consice description of the change in the body in bullet points."
+                                .. "Use as litte bullet points as possible. We want to keep the commit message short and concise."
+                                .. "If you encounter variable names or other code elements, please wrap them in backticks. (e.g. `<var-name>`)"
+                                .. "When asserting the scope, consider if it makes most sense to include the file name or the folder scope by looking at the pathes of the files changed."
+                                .. "In most cases, the summary is enough to describe the overall change. Use the body only if the summary is not enough to describe the change."
+                                .. "If the commit affects multiple files, consider the pathes of the files changed for the scope."
+                                .. "If the commit only affects a single file, or one file has the main role in the change, please use the following format:"
+                                .. "- feat|fix|docs|style|refactor|test|chore|revert(<affected-file (without file-extension) or scope>): description"
+                                .. "In the bullet points, use the following format:"
+                                .. "- feat|fix|docs|style|refactor|test|chore|revert(<affected-file (without file-extension) or scope>): description"
+
+                            local template = base_template
+
+                            -- If the branch name includes an issue ticket number, we use it to prefix the commit message.
+                            -- Example: bcd-1234-some-new-feature -> BCD-1234 feat: some new feature
+                            local current_branch = vim.fn.system("git rev-parse --abbrev-ref HEAD")
+                            local issue_id = string.match(current_branch, "^bcd%-(%d%d%d%d)")
+                            local prefix = issue_id and "BCD-" .. issue_id .. " " or ""
+
+                            if issue_id then
+                                template = base_template
+                                    .. "\n\n"
+                                    .. "Please prefix the summary line with the following issue ID: "
+                                    .. prefix
+                                    .. "\n\n"
+                                    .. "Example: BCD-1234 feat: some new feature"
+                            end
+
+                            return template
+                        end,
+                    },
+                    {
+                        role = "user",
+                        content = function()
+                            return "This is the diff I have staged:"
+                                .. "\n\n```diff\n"
+                                .. vim.fn.system("git diff --cached")
                                 .. "\n```"
                         end,
                     },
@@ -63,8 +98,8 @@ M.spec = {
         },
     },
     keys = {
-        { "<leader>ia", "<cmd>CodeCompanionActions<CR>", desc = "AI Actions" },
-        { "<leader>it", "<cmd>CodeCompanionToggle<CR>", desc = "AI Actions" },
+        { "<leader>ia", "<cmd>CodeCompanionActions<CR>", desc = "Actions" },
+        { "<leader>it", "<cmd>CodeCompanionToggle<CR>", desc = "Toggle" },
         { "ga", "<cmd>CodeCompanionAdd<CR>", desc = "Add Selection", mode = { "v" } },
     },
     init = function()
