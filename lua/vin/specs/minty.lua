26ba1e630aef06c11954524774731f14361ee8d9
nbr <nikolaus.brunner@protonmail.ch>
2024-10-24 18:53:30 +0200

    deps(minty): add save mappings

--- lua/vin/specs/minty.lua
+++ lua/vin/specs/minty.lua
@@ -2,6 +2,22 @@
 return {
     "nvchad/minty",
     dependencies = { "nvchad/volt" },
+    opts = {
+        huefy = {
+
+            mappings = function(bufs) -- bufs is a table cuz 2 bufs!
+                -- local api = require("minty.shades.api")
+                -- vim.keymap.set("n", "s", api.save_color, { buffer = buf })
+            end,
+        },
+
+        shades = {
+            mappings = function(buf)
+                local api = require("minty.shades.api")
+                vim.keymap.set("n", "<C-s>", api.save_color, { buffer = buf })
+            end,
+        },
+    },
     keys = {
         {
             "<leader>cph",
