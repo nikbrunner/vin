82c75407ded12a18ae855e2b0e21aefe06c18187
nbr <nikolaus.brunner@protonmail.ch>
2024-10-27 10:36:59 +0100

    style(catppuccin): refactor module structure and adjust color highlights
    - refactor: simplify module export by removing intermediate `M` table
    - style: update custom highlight groups to use consistent `mantle` background
    - chore: add `priority` setting for theme loading

--- lua/vin/specs/catppuccin.lua
+++ lua/vin/specs/catppuccin.lua
@@ -1,33 +1,31 @@
-local M = {}
-
 ---@type LazyPluginSpec
-M.spec = {
+return {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
+    priority = 1000,
     ---@module "catppuccin"
     ---@type CatppuccinOptions
     opts = {
         term_colors = true,
-        color_overrides = {
-            all = {
-                -- mantle = "#0e1313",
-                -- base = "#121818",
-                -- crust = "#1c2424",
-                -- mauve = "#9dafa6",
-                -- blue = "#8fa7bc",
-            },
-        },
+        -- color_overrides = {
+        --     all = {
+        --         mantle = "#182121",
+        --         base = "#1c2626",
+        --         crust = "#273232",
+        --         mauve = "#9dafa6",
+        --         blue = "#8fa7bc",
+        --     },
+        -- },
         custom_highlights = function(colors)
             return {
-                YaziFloat = { link = "NormalFloat" },
-                FzfLuaNormal = { link = "NormalFloat" },
-                FzfLuaBorder = { link = "FloatBorder" },
-                FzfLuaPreviewBorder = { link = "FloatBorder" },
+                FloatBorder = { bg = colors.mantle },
+                FloatTitle = { bg = colors.mantle },
+                FzfLuaNormal = { bg = colors.mantle },
+                FzfLuaBorder = { bg = colors.mantle },
+                YaziFloat = { bg = colors.mantle },
                 TreesitterContextBottom = { sp = colors.surface0, style = false },
             }
         end,
     },
 }
-
-return M.spec
