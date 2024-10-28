640cab05475eeee943344ddfe158b045f5c2d095
nbr <nikolaus.brunner@protonmail.ch>
2024-10-28 22:12:02 +0100

    feat(blink): enhance keybindings and completion settings
    - feat(blink): add `<C-e>` and `<C-y>` as hide/accept alternatives
    - feat(blink): add window customization for autocomplete
    - fix(blink): disable nvim-cmp default highlighting

--- lua/vin/specs/blink.lua
+++ lua/vin/specs/blink.lua
@@ -10,8 +10,8 @@ return {
     opts = {
         keymap = {
             show = "<C-l>",
-            hide = { "<S-CR>", "<Left>" },
-            accept = { "<CR>", "<Right>" },
+            hide = { "<S-CR>", "<C-e>", "<Left>" },
+            accept = { "<CR>", "<C-y>", "<Right>" },
             select_prev = { "<Up>" },
             select_next = { "<Down>" },
 
@@ -28,8 +28,20 @@ return {
                 enabled = true,
             },
         },
+        windows = {
+            autocomplete = {
+                min_width = 15,
+                max_height = 10,
+                border = "solid",
+                -- Controls how the completion items are selected
+                -- 'preselect' will automatically select the first item in the completion list
+                -- 'manual' will not select any item by default
+                -- 'auto_insert' will not select any item by default, and insert the completion items automatically when selecting them
+                selection = "auto_insert",
+            },
+        },
         highlight = {
-            use_nvim_cmp_as_default = true,
+            use_nvim_cmp_as_default = false,
         },
     },
 }
