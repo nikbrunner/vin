845a454f63abc4ec5efa5191c1572193db3ae33a
nbr <nikolaus.brunner@protonmail.ch>
2024-10-31 17:50:58 +0100

    feat(undotree): add undotree plugin with toggle keybinding

---
+++ lua/vin/specs/undotree.lua
@@ -0,0 +1,11 @@
+return {
+    "mbbill/undotree",
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>U",
+            "<cmd>UndotreeToggle<CR>",
+            desc = "Undotree",
+        },
+    },
+}
