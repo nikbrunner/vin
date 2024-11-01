950e3a0a9721699955d8e4ffdb02c657bdf47e6e
nbr <nikolaus.brunner@protonmail.ch>
2024-11-01 13:46:12 +0100

    feat(qmk): improve keymap formatting with auto-detection
    - feat(qmk): add dynamic layout detection for CRKBD and Moonlander keyboards
    - feat(qmk): add `KC_TRANSPARENT` and `SS_FATARROW` keymap overrides
    - refactor(qmk): move layout configuration into file-specific auto commands

--- lua/vin/specs/qmk.lua
+++ lua/vin/specs/qmk.lua
@@ -4,24 +4,60 @@ local M = {}
 M.spec = {
     "codethread/qmk.nvim",
     ft = "c",
-    ---@type qmk.UserConfig
+    event = "VeryLazy",
     opts = {
-        name = "LAYOUT_split_3x5_3",
-        layout = {
-            "x x x x x _ x x x x x",
-            "x x x x x _ x x x x x",
-            "x x x x x _ x x x x x",
-            "_ _ x x x _ x x x _ _",
-        },
         comment_preview = {
             position = "inside",
             keymap_overrides = {
+                KC_TRANSPARENT = " ",
                 ["SS_TILD_SLSH"] = "~/",
+                ["SS_FATARROW"] = "=>",
             },
         },
     },
-    config = function(_, opts)
-        require("qmk").setup(opts)
+    config = function(_, sharedOpts)
+        local setup = function(opts)
+            local mergedOpts = vim.tbl_deep_extend("force", sharedOpts, opts or {})
+            require("qmk").setup(mergedOpts)
+        end
+
+        local group = vim.api.nvim_create_augroup("qmk-auto-format-group", {})
+
+        vim.api.nvim_create_autocmd("BufEnter", {
+            desc = "Format CRKBD koyo keymap",
+            group = group,
+            pattern = "*nikbrunner/qmk_firmware/keyboards/crkbd/keymaps/koyo/keymap.c",
+            callback = function()
+                setup({
+                    name = "LAYOUT_split_3x5_3",
+                    layout = {
+                        "x x x x x _ x x x x x",
+                        "x x x x x _ x x x x x",
+                        "x x x x x _ x x x x x",
+                        "_ _ x x x _ x x x _ _",
+                    },
+                })
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("BufEnter", {
+            desc = "Format Moonlander koyo keymap",
+            group = group,
+            pattern = "*nikbrunner/koyo-oryx-qmk/EM5en/keymap.c",
+            callback = function()
+                setup({
+                    name = "LAYOUT_moonlander",
+                    layout = {
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x _ _ _ x x x x x x",
+                        "x x x x x x _ _ _ x x x x x x",
+                        "_ _ _ _ x x x _ x x x _ _ _ _",
+                    },
+                })
+            end,
+        })
     end,
 }
 
