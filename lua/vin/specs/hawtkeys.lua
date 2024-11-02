7d7dffae992210bf995399586e7fe87c24677c7e
nbr <nikolaus.brunner@protonmail.ch>
2024-11-02 12:32:26 +0100

    feat(specs): add hawtkeys plugin for keybinding analysis

---
+++ lua/vin/specs/hawtkeys.lua
@@ -0,0 +1,9 @@
+return {
+    "tris203/hawtkeys.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    event = "VeryLazy",
+    opts = {},
+}
