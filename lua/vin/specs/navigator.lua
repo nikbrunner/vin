5b6d4f903a8880bbb4f0f3875329230e656dd9f8
nbr <nikolaus.brunner@protonmail.ch>
2024-11-06 20:58:57 +0100

    refactor(navigator): simplify key mappings definition

--- lua/vin/specs/navigator.lua
+++ lua/vin/specs/navigator.lua
@@ -4,26 +4,10 @@ local M = {}
 M.spec = {
     "numToStr/Navigator.nvim",
     keys = {
-        {
-            "<C-h>",
-            "<CMD>NavigatorLeft<CR>",
-            mode = { "n", "t" },
-        },
-        {
-            "<C-l>",
-            "<CMD>NavigatorRight<CR>",
-            mode = { "n", "t" },
-        },
-        {
-            "<C-k>",
-            "<CMD>NavigatorUp<CR>",
-            mode = { "n", "t" },
-        },
-        {
-            "<C-j>",
-            "<CMD>NavigatorDown<CR>",
-            mode = { "n", "t" },
-        },
+        { "<C-h>", "<CMD>NavigatorLeft<CR>" },
+        { "<C-l>", "<CMD>NavigatorRight<CR>" },
+        { "<C-k>", "<CMD>NavigatorUp<CR>" },
+        { "<C-j>", "<CMD>NavigatorDown<CR>" },
     },
     config = function(_, opts)
         require("Navigator").setup(opts)
