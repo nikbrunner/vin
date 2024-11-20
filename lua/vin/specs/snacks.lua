7b79bb453d8b63ad7e97e8b8248e36842d7d29dd
nbr <nikolaus.brunner@protonmail.ch>
2024-11-20 18:02:41 +0100

    feat(snacks): setup dashboard

--- lua/vin/specs/snacks.lua
+++ lua/vin/specs/snacks.lua
@@ -1,5 +1,24 @@
+local M = {}
+
+M.headers = {
+    vin = [[
+    ██╗   ██╗██╗███╗   ██╗
+    ██║   ██║██║████╗  ██║
+    ██║   ██║██║██╔██╗ ██║
+    ╚██╗ ██╔╝██║██║╚██╗██║
+     ╚████╔╝ ██║██║ ╚████║
+      ╚═══╝  ╚═╝╚═╝  ╚═══╝]],
+    nbr = [[
+    ███╗   ██╗██████╗ ██████╗ 
+    ████╗  ██║██╔══██╗██╔══██╗
+    ██╔██╗ ██║██████╔╝██████╔╝
+    ██║╚██╗██║██╔══██╗██╔══██╗
+    ██║ ╚████║██████╔╝██║  ██║
+    ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝]],
+}
+
 ---@type LazyPluginSpec
-return {
+M.spec = {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
@@ -10,6 +29,53 @@ return {
         debug = { enabled = true },
         notifier = { enabled = true },
         toggle = { enabled = true },
+        dashboard = {
+            preset = {
+                -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+                ---@type fun(cmd:string, opts:table)|nil
+                pick = nil,
+                -- Used by the `keys` section to show keymaps.
+                -- Set your curstom keymaps here.
+                -- When using a function, the `items` argument are the default keymaps.
+                ---@type snacks.dashboard.Item[]
+                keys = {
+                    { icon = " ", key = ",", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "f", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                    {
+                        icon = " ",
+                        key = "c",
+                        desc = "Config",
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                    },
+                    { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+                header = M.headers.nbr,
+            },
+
+            sections = {
+                { section = "header" },
+                { section = "keys", gap = 1, padding = 1 },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = vim.fn.isdirectory(".git") == 1,
+                    cmd = "hub status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
+                { section = "startup" },
+            },
+        },
         terminal = {
             win = {
                 border = "solid",
@@ -86,3 +152,5 @@ return {
         })
     end,
 }
+
+return M.spec
