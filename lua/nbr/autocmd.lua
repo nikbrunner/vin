158affa298426981a24c0ccb4f0db9f35458229c
nbr <nikolaus.brunner@protonmail.ch>
2024-12-02 16:52:19 +0100

    feat(autocmd): add LSP progress notifications with spinner animation

--- lua/nbr/autocmd.lua
+++ lua/nbr/autocmd.lua
@@ -201,3 +201,19 @@ auto("FocusGained", {
         end
     end,
 })
+
+auto("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+        vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
+            id = "lsp_progress",
+            title = "LSP Progress",
+            opts = function(notif)
+                notif.icon = ev.data.params.value.kind == "end" and " "
+                    ---@diagnostic disable-next-line: undefined-field
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
