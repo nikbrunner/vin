adcba7b0198db3d0a5de271ed3fcd55f533268f0
nbr <nikolaus.brunner@protonmail.ch>
2024-12-19 19:46:01 +0100

    style(config): change colorscheme to terra-winter-night

--- lua/nbr/config.lua
+++ lua/nbr/config.lua
@@ -2,7 +2,7 @@ local M = {}
 
 ---@class VinConfig
 M.config = {
-    colorscheme = "tokyonight-moon",
+    colorscheme = "terra-winter-night",
     ---@type "dark" | "light"
     background = "dark",
     ---see for wezterm themes: https://wezfurlong.org/wezterm/colorschemes/index.html
